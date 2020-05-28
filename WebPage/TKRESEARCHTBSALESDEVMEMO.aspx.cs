f2d5adbc873bb8a8a419e21b98765cc67fcfa666
TKFood <tk290@tkfood.com.tw>
2020-05-28 16:52:46 +0800

    ADD TKRESEARCHTBSALESDEVMEMO

---
+++ WebPage/TKRESEARCHTBSALESDEVMEMO.aspx.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Ede.Uof.Utility.Data;
+using Ede.Uof.Utility.Page.Common;
+
+public partial class CDS_WebPage_TKRESEARCHTBSALESDEVMEMO : Ede.Uof.Utility.Page.BasePage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        //用ExpandoObject物件傳遞參數
+        //ExpandoObject param = new { Name = "asd" }.ToExpando();
+        //ExpandoObject param = new { Name = txtParam.Text }.ToExpando();
+
+        //因為執行此行後，才會把JS的Event註冊到頁面上，所以過此行後下一次按btn元件的Event才會開窗並傳參數
+        //故Dialog.Open2適合於參數為固定式的
+        //Dialog.Open2(btn, "~/CDS/WebPage/Dialog.aspx", "", 800, 600, Dialog.PostBackType.Allows, param);
+
+        BindGrid();
+
+        //if (!IsPostBack)
+        //{
+        //    BindGrid();
+        //}
+       
+
+    }
+
+    #region FUNCTION
+    private void BindGrid()
+    {
+        //建立Grid資料
+        DataSet ds = new DataSet();
+        DatabaseHelper DbQuery = new DatabaseHelper();
+        DataTable dt = new DataTable();
+
+        //資源來源-用SqlCommand +SqlDataAdapter +DataTable 來查詢
+        string connectionString = ConfigurationManager.ConnectionStrings["ERPconnectionstring"].ToString();
+        using (SqlConnection conn = new SqlConnection(connectionString))
+        {
+            SqlCommand command = new SqlCommand("SELECT [ID],[SERNO],[STATUS],[CLIENT],[PROD],[PRICES],[PROMOTIONS],[SPEC],[VALIDDATES],[PLACES],[ONSALESDATES],[PRODESGIN],[ASSESSMENT],[COSTS],[SALESPRICES],[TESTDATES],[OWNER],[MEMO] FROM [TKRESEARCH].[dbo].[TBSALESDEVMEMO]", conn);
+            ds.Clear();
+
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            conn.Open();
+
+            adapter.Fill(ds, command.ToString());
+        }
+
+        Grid1.DataSource = ds.Tables[0];
+        Grid1.DataBind();
+    }
+
+    protected void Grid1_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+        if (e.Row.RowType == DataControlRowType.DataRow)
+        {
+            DataRowView row = (DataRowView)e.Row.DataItem;
+            LinkButton lbtnName = (LinkButton)e.Row.FindControl("lbtnName");
+
+            ExpandoObject param = new { ID = row["ID"].ToString() }.ToExpando();
+
+            //Grid開窗是用RowDataBound事件再開窗
+            Dialog.Open2(lbtnName, "~/CDS/WebPage/TKWEBDialogEDITDEL.aspx", "", 800, 600, Dialog.PostBackType.AfterReturn, param);
+        }
+    }
+
+    #endregion
+
+    #region BUTTON
+    protected void btn_Click(object sender, EventArgs e)
+    {
+
+
+        //開窗後回傳參數
+        if (!string.IsNullOrEmpty(Dialog.GetReturnValue()))
+        {
+            //txtReturnValue.Text = Dialog.GetReturnValue();
+        }
+
+
+    }
+
+    protected void btn3_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btn4_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btn5_Click(object sender, EventArgs e)
+    {
+        if(!string.IsNullOrEmpty(Dialog.GetReturnValue()))
+        {
+            if (Dialog.GetReturnValue().Equals("NeedPostBack"))
+            {
+                BindGrid();
+            }
+
+        }
+    }
+
+    #endregion
+}
---
+++ WebPage/TKRESEARCHTBSALESDEVMEMODialogADD.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master/DialogMasterPage.master" AutoEventWireup="true" CodeFile="TKRESEARCHTBSALESDEVMEMODialogADD.aspx.cs" Inherits="CDS_WebPage_TKRESEARCHTBSALESDEVMEMODialogADD" %>
+
+<%@ Register Src="~/Common/HtmlEditor/UC_HtmlEditor.ascx" TagPrefix="uc1" TagName="UC_HtmlEditor" %>
+
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+
+<script>
+        
+</script>
+
+    <table class="PopTable" >
+        <tr>
+            <td class="PopTableLeftTD" >
+                <asp:Label ID="Label1" runat="server" Text="CDS_WebPage_TKRESEARCHTBSALESDEVMEMODialogADD"></asp:Label>
+            </td>
+            <td class="PopTableRightTD" colspan=2>
+                <asp:Label ID="lblParam" runat="server" Text=""></asp:Label>
+            </td>            
+        </tr>        
+          <tr>
+            <td class="PopTableLeftTD" >
+                <asp:Label ID="Label8" runat="server" Text="金額"></asp:Label>
+            </td>
+            <td class="PopTableRightTD">
+                <asp:TextBox ID="txtTextBox6" runat="server"  Text="10000"></asp:TextBox>
+            </td> 
+       </tr>
+
+    </table>
+
+ 
+</asp:Content>
+
