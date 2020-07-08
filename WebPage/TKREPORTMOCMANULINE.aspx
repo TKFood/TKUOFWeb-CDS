1c1d2b4ceb5eeb290b05aadd8178ede41b50e983
TKFood <tk290@tkfood.com.tw>
2020-07-08 14:08:17 +0800

    ADD TKREPORTMOCMANULINE

---
+++ WebPage/TKREPORTMOCMANULINE.aspx
@@ -0,0 +1,65 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master/DefaultMasterPage.master" AutoEventWireup="true" CodeFile="TKREPORTMOCMANULINE.aspx.cs" Inherits="CDS_WebPage_TKREPORTMOCMANULINE" %>
+<%@ Register Assembly="Ede.Uof.Utility.Component.Grid" Namespace="Ede.Uof.Utility.Component" TagPrefix="Fast" %>
+
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.4/themes/trontastic/jquery-ui.css">
+
+<script>    
+    $(function () {
+        $("#<%= txtDate1.ClientID %>").datepicker({ dateFormat: "yymmdd", });
+        $("#<%= txtDate2.ClientID %>").datepicker({ dateFormat: "yymmdd", });      
+    });
+
+</script>
+
+    <table class="PopTable" >         
+         <tr>
+            <td class="PopTableLeftTD">
+                <asp:Label ID="Label2" runat="server" Text="日期:" meta:resourcekey="Label4Resource1"></asp:Label>             
+            </td>
+            <td class="PopTableRightTD">                    
+                <asp:TextBox ID="txtDate1"  runat="server" Width = "100px"></asp:TextBox>
+                <asp:Label ID="Label11" runat="server" Text="~"></asp:Label>
+                <asp:TextBox ID="txtDate2"  runat="server" Width = "100px"></asp:TextBox>
+                <asp:Label ID="Label12" runat="server" Text=" "></asp:Label>
+                <asp:Button ID="Button1" runat="server" Text=" 查詢 "
+                    onclick="btn1_Click" meta:resourcekey="btn4Resource1" />                
+            </td>            
+        </tr>
+    </table>
+    <table class="PopTable">
+            <td colspan="2" class="PopTableRightTD" >
+                <div style="overflow-x:auto;width:100%">
+                    <Fast:Grid ID="Grid1" OnRowDataBound="Grid1_RowDataBound" runat="server"  OnBeforeExport="OnBeforeExport" AllowPaging="true"  AutoGenerateCheckBoxColumn="False" AllowSorting="True" AutoGenerateColumns="False" CustomDropDownListPage="False" DataKeyOnClientWithCheckBox="False" DefaultSortDirection="Ascending" EmptyDataText="No data found" EnhancePager="True" KeepSelectedRows="False" PageSize="1000" SelectedRowColor="" UnSelectedRowColor="" meta:resourcekey="Grid1Resource1" OnPageIndexChanging="grid_PageIndexChanging" >
+                        <EnhancePagerSettings FirstImageUrl="" FirstAltImageUrl="" PreviousImageUrl="" NextImageUrl="" LastImageUrl="" LastAltImage="" PageNumberCssClass="" PageNumberCurrentCssClass="" PageInfoCssClass="" PageRedirectCssClass="" NextIAltImageUrl="" PreviousAltImageUrl="" ShowHeaderPager="True"></EnhancePagerSettings>
+                 
+                            <ExportExcelSettings AllowExportToExcel="true" ExportType="DataSource" ></ExportExcelSettings>
+                            <Columns>
+                                 <asp:BoundField HeaderText="線別" DataField="MANU" ItemStyle-Width="100px" HeaderStyle-HorizontalAlign="Center"  ItemStyle-HorizontalAlign="Center" >
+                                    <ItemStyle HorizontalAlign="Center" Width="200px"></ItemStyle>
+                                </asp:BoundField>   
+                                 <asp:BoundField HeaderText="客戶" DataField="CLINET" ItemStyle-Width="100px" HeaderStyle-HorizontalAlign="Center"  ItemStyle-HorizontalAlign="Center" >
+                                    <ItemStyle HorizontalAlign="Left" Width="200px"></ItemStyle>
+                                </asp:BoundField>   
+                                 <asp:BoundField HeaderText="預計生產日期" DataField="MANUDATE" ItemStyle-Width="100px" HeaderStyle-HorizontalAlign="Center"  ItemStyle-HorizontalAlign="Center" >
+                                    <ItemStyle HorizontalAlign="Left" Width="100px"></ItemStyle>
+                                </asp:BoundField>   
+                                 <asp:BoundField HeaderText="品名" DataField="MB002" ItemStyle-Width="100px" HeaderStyle-HorizontalAlign="Center"  ItemStyle-HorizontalAlign="Center" >
+                                    <ItemStyle HorizontalAlign="Left" Width="400px"></ItemStyle>
+                                </asp:BoundField>   
+                                <asp:BoundField HeaderText="包裝數" DataField="PACKAGE" ItemStyle-Width="100px" HeaderStyle-HorizontalAlign="Center"  ItemStyle-HorizontalAlign="Center" >
+                                    <ItemStyle HorizontalAlign="Center" Width="200px"></ItemStyle>
+                                </asp:BoundField> 
+                                 <asp:BoundField HeaderText="重量" DataField="NUM" ItemStyle-Width="100px" HeaderStyle-HorizontalAlign="Center"  ItemStyle-HorizontalAlign="Center" >
+                                    <ItemStyle HorizontalAlign="Center" Width="200px"></ItemStyle>
+                                </asp:BoundField> 
+                           
+                            </Columns>
+                        </Fast:Grid>
+                    </div>
+            </td>
+        </tr>
+    </table>
+</asp:Content>
+
---
+++ WebPage/TKREPORTMOCMANULINE.aspx.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Ede.Uof.Utility.Data;
+using Ede.Uof.Utility.Page.Common;
+
+public partial class CDS_WebPage_TKREPORTMOCMANULINE : Ede.Uof.Utility.Page.BasePage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        //用ExpandoObject物件傳遞參數
+        //ExpandoObject param = new { Name = "asd" }.ToExpando();
+        //ExpandoObject param = new { Name = txtParam.Text }.ToExpando();
+
+        //因為執行此行後，才會把JS的Event註冊到頁面上，所以過此行後下一次按btn元件的Event才會開窗並傳參數
+        //故Dialog.Open2適合於參數為固定式的
+        //Dialog.Open2(btn, "~/CDS/WebPage/Dialog.aspx", "", 800, 600, Dialog.PostBackType.Allows, param);
+
+
+
+
+        if (!IsPostBack)
+        {
+            BindGrid(DateTime.Now.ToString("yyyyMMdd"), DateTime.Now.AddMonths(1).AddDays(-DateTime.Now.AddMonths(1).Day).ToString("yyyyMMdd"));
+
+            txtDate1.Text = DateTime.Now.ToString("yyyyMMdd");
+            txtDate2.Text = DateTime.Now.AddMonths(1).AddDays(-DateTime.Now.AddMonths(1).Day).ToString("yyyyMMdd");
+
+        }
+        else
+        {
+            BindGrid(txtDate1.Text, txtDate2.Text);
+        }
+
+
+
+    }
+
+    #region FUNCTION
+    
+    private void BindGrid(string SDATE,string EDATE)
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["ERPconnectionstring"].ToString();
+        Ede.Uof.Utility.Data.DatabaseHelper m_db = new Ede.Uof.Utility.Data.DatabaseHelper(connectionString);
+
+        string cmdTxt = @" SELECT [MANU],[CLINET],CONVERT(NVARCHAR,[MANUDATE],112) AS MANUDATE,[MB002],CONVERT(INT,(ISNULL([PACKAGE],0))) AS PACKAGE,ISNULL([NUM],0) AS NUM
+                            FROM [TKMOC].[dbo].[MOCMANULINE]
+                            WHERE CONVERT(NVARCHAR,[MANUDATE],112)>=@SDATE AND  CONVERT(NVARCHAR,[MANUDATE],112)<=@EDATE
+                            AND (NUM>0 OR [PACKAGE]>0)
+                            ORDER BY [MANU],[MANUDATE],[CLINET],[MB002]
+
+                        ";
+
+        m_db.AddParameter("@SDATE", SDATE);
+        m_db.AddParameter("@EDATE", EDATE);
+
+        DataTable dt = new DataTable();
+
+        dt.Load(m_db.ExecuteReader(cmdTxt));
+
+        Grid1.DataSource = dt;
+        Grid1.DataBind();
+    }
+
+    protected void grid_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        Grid1.PageIndex = e.NewPageIndex;
+        BindGrid(this.Session["SDATE"].ToString(), this.Session["EDATE"].ToString());
+    }
+    protected void Grid1_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+    //    if (e.Row.RowType == DataControlRowType.DataRow)
+    //    {
+    //        DataRowView row = (DataRowView)e.Row.DataItem;
+    //        LinkButton lbtnName = (LinkButton)e.Row.FindControl("lbtnName");
+
+    //        ExpandoObject param = new { ID = row["ID"].ToString() }.ToExpando();
+
+    //        //Grid開窗是用RowDataBound事件再開窗
+    //        Dialog.Open2(lbtnName, "~/CDS/WebPage/TKRESEARCHTBDEVMEMODialogEDITDEL.aspx", "", 800, 600, Dialog.PostBackType.AfterReturn, param);
+    //    }
+
+
+    }
+
+    public void OnBeforeExport(object sender,Ede.Uof.Utility.Component.BeforeExportEventArgs e)
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["ERPconnectionstring"].ToString();
+        Ede.Uof.Utility.Data.DatabaseHelper m_db = new Ede.Uof.Utility.Data.DatabaseHelper(connectionString);
+
+        string cmdTxt = @" SELECT [MANU],[CLINET],CONVERT(NVARCHAR,[MANUDATE],112) AS MANUDATE,[MB002],CONVERT(INT,(ISNULL([PACKAGE],0))) AS PACKAGE,ISNULL([NUM],0) AS NUM
+                            FROM [TKMOC].[dbo].[MOCMANULINE]
+                            WHERE CONVERT(NVARCHAR,[MANUDATE],112)>=@SDATE AND  CONVERT(NVARCHAR,[MANUDATE],112)<=@EDATE
+                            AND (NUM>0 OR [PACKAGE]>0)
+                            ORDER BY [MANU],[MANUDATE],[CLINET],[MB002]
+
+                        ";
+
+
+        m_db.AddParameter("@SDATE", txtDate1.Text);
+        m_db.AddParameter("@EDATE", txtDate2.Text);
+
+        DataTable dt = new DataTable();
+
+        dt.Load(m_db.ExecuteReader(cmdTxt));
+
+        if (dt.Rows.Count>0)
+        {
+            dt.Columns[0].Caption = "線別";
+            dt.Columns[1].Caption = "客戶";
+            dt.Columns[2].Caption = "預計生產日期";
+            dt.Columns[3].Caption = "品名";
+            dt.Columns[4].Caption = "包裝數";
+            dt.Columns[5].Caption = "重量";
+
+            e.Datasource = dt;
+        }
+    }
+    #endregion
+
+    #region BUTTON
+    protected void btn_Click(object sender, EventArgs e)
+    {
+
+
+        //開窗後回傳參數
+        if (!string.IsNullOrEmpty(Dialog.GetReturnValue()))
+        {
+            //txtReturnValue.Text = Dialog.GetReturnValue();
+        }
+
+
+    }
+
+
+    protected void btn1_Click(object sender, EventArgs e)
+    {
+        //this.Session["SDATE"] = txtDate1.Text.Trim();
+        //this.Session["EDATE"] = txtDate2.Text.Trim();
+    }
+
+    #endregion
+}
