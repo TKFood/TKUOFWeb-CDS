72fc4adcb30721c574583b1668852495a2e6b51c
TKFood <tk290@tkfood.com.tw>
2020-08-05 10:47:07 +0800

    add TKREPORTTB_EIP_SCH_WORK

---
+++ WebPage/TKREPORTTB_EIP_SCH_WORK.aspx.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Ede.Uof.Utility.Data;
+using Ede.Uof.Utility.Page.Common;
+
+public partial class CDS_WebPage_TKREPORTTB_EIP_SCH_WORK : Ede.Uof.Utility.Page.BasePage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        //用ExpandoObject物件傳遞參數
+        //ExpandoObject param = new { Name = "asd" }.ToExpando();
+        //ExpandoObject param = new { Name = txtParam.Text }.ToExpando();
+
+        //因為執行此行後，才會把JS的Event註冊到頁面上，所以過此行後下一次按btn元件的Event才會開窗並傳參數
+        //故Dialog.Open2適合於參數為固定式的
+        //Dialog.Open2(btn, "~/CDS/WebPage/Dialog.aspx", "", 800, 600, Dialog.PostBackType.Allows, param);
+
+
+
+
+        if (!IsPostBack)
+        {
+            BindGrid();
+
+        }
+        else
+        {
+            BindGrid();
+        }
+
+
+
+    }
+
+    #region FUNCTION
+    
+    private void BindGrid()
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["connectionstring"].ToString();
+        Ede.Uof.Utility.Data.DatabaseHelper m_db = new Ede.Uof.Utility.Data.DatabaseHelper(connectionString);
+
+        string cmdTxt = @" SELECT 
+                            USER1.[NAME] AS 'NAME1',[TB_EIP_SCH_WORK].[SUBJECT],CONVERT(NVARCHAR,[TB_EIP_SCH_WORK].[END_TIME],111)  AS END_TIME,DATEDIFF(day, [TB_EIP_SCH_WORK].[END_TIME],GETDATE()) AS DIFFDATES,USER2.[NAME]  AS 'NAME2'
+                            ,CASE WHEN [TB_EIP_SCH_WORK].[WORK_STATE]='NotYetBegin' THEN '當未開始' ELSE '進行中' END AS 'STATUS'
+                            ,[TB_EIP_SCH_WORK].[WORK_STATE],[TB_EIP_SCH_WORK].[EXECUTE_USER],[TB_EIP_SCH_WORK].[SOURCE_USER]
+                            FROM [UOF].[dbo].[TB_EIP_SCH_WORK]
+                            LEFT JOIN [UOF].[dbo].[TB_EB_USER] USER1 ON USER1.USER_GUID=[TB_EIP_SCH_WORK].[EXECUTE_USER]
+                            LEFT JOIN [UOF].[dbo].[TB_EB_USER] USER2 ON USER2.USER_GUID=[TB_EIP_SCH_WORK].[SOURCE_USER]
+                            WHERE [WORK_STATE] IN ('NotYetBegin','Proceeding')
+                            AND DATEDIFF(day, [TB_EIP_SCH_WORK].[END_TIME],GETDATE())>=0
+                            ORDER BY [EXECUTE_USER],[TB_EIP_SCH_WORK].[END_TIME],[SUBJECT]
+
+                        ";
+
+    
+
+        DataTable dt = new DataTable();
+
+        dt.Load(m_db.ExecuteReader(cmdTxt));
+
+        Grid1.DataSource = dt;
+        Grid1.DataBind();
+    }
+
+    protected void grid_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        Grid1.PageIndex = e.NewPageIndex;
+        BindGrid();
+    }
+    protected void Grid1_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+    //    if (e.Row.RowType == DataControlRowType.DataRow)
+    //    {
+    //        DataRowView row = (DataRowView)e.Row.DataItem;
+    //        LinkButton lbtnName = (LinkButton)e.Row.FindControl("lbtnName");
+
+    //        ExpandoObject param = new { ID = row["ID"].ToString() }.ToExpando();
+
+    //        //Grid開窗是用RowDataBound事件再開窗
+    //        Dialog.Open2(lbtnName, "~/CDS/WebPage/TKRESEARCHTBDEVMEMODialogEDITDEL.aspx", "", 800, 600, Dialog.PostBackType.AfterReturn, param);
+    //    }
+
+
+    }
+
+    public void OnBeforeExport(object sender,Ede.Uof.Utility.Component.BeforeExportEventArgs e)
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["connectionstring"].ToString();
+        Ede.Uof.Utility.Data.DatabaseHelper m_db = new Ede.Uof.Utility.Data.DatabaseHelper(connectionString);
+
+        string cmdTxt = @" SELECT 
+                            USER1.[NAME] AS 'NAME1',[TB_EIP_SCH_WORK].[SUBJECT],CONVERT(NVARCHAR,[TB_EIP_SCH_WORK].[END_TIME],111)  AS END_TIME,DATEDIFF(day, [TB_EIP_SCH_WORK].[END_TIME],GETDATE()) AS DIFFDATES,USER2.[NAME]  AS 'NAME2'
+                            ,CASE WHEN [TB_EIP_SCH_WORK].[WORK_STATE]='NotYetBegin' THEN '當未開始' ELSE '進行中' END AS 'STATUS'
+                            ,[TB_EIP_SCH_WORK].[WORK_STATE],[TB_EIP_SCH_WORK].[EXECUTE_USER],[TB_EIP_SCH_WORK].[SOURCE_USER]
+                            FROM [UOF].[dbo].[TB_EIP_SCH_WORK]
+                            LEFT JOIN [UOF].[dbo].[TB_EB_USER] USER1 ON USER1.USER_GUID=[TB_EIP_SCH_WORK].[EXECUTE_USER]
+                            LEFT JOIN [UOF].[dbo].[TB_EB_USER] USER2 ON USER2.USER_GUID=[TB_EIP_SCH_WORK].[SOURCE_USER]
+                            WHERE [WORK_STATE] IN ('NotYetBegin','Proceeding')
+                            AND DATEDIFF(day, [TB_EIP_SCH_WORK].[END_TIME],GETDATE())>=0
+                            ORDER BY [EXECUTE_USER],[TB_EIP_SCH_WORK].[END_TIME],[SUBJECT]
+
+                        ";
+
+
+
+
+        DataTable dt = new DataTable();
+
+        dt.Load(m_db.ExecuteReader(cmdTxt));
+
+        if (dt.Rows.Count>0)
+        {
+            dt.Columns[0].Caption = "被交辨人";
+            dt.Columns[1].Caption = "交辨事項";
+            dt.Columns[2].Caption = "預計完成日";
+            dt.Columns[3].Caption = "延遲天數";
+            dt.Columns[4].Caption = "交辨人";
+            dt.Columns[5].Caption = "狀態";
+
+
+            e.Datasource = dt;
+        }
+    }
+    #endregion
+
+    #region BUTTON
+    protected void btn_Click(object sender, EventArgs e)
+    {
+
+
+        //開窗後回傳參數
+        if (!string.IsNullOrEmpty(Dialog.GetReturnValue()))
+        {
+            //txtReturnValue.Text = Dialog.GetReturnValue();
+        }
+
+
+    }
+
+
+    protected void btn1_Click(object sender, EventArgs e)
+    {
+        //this.Session["SDATE"] = txtDate1.Text.Trim();
+        //this.Session["EDATE"] = txtDate2.Text.Trim();
+    }
+
+    #endregion
+}
