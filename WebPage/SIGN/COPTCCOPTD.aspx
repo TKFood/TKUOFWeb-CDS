8f0026d0e7e6b79e5ac143cccd64b8dd1410772c
TKFood <tk290@tkfood.com.tw>
2020-11-17 13:53:01 +0800

    BindGrid(txtDate1.Text);

---
+++ WebPage/SIGN/COPTCCOPTD.aspx
@@ -0,0 +1,86 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master/DefaultMasterPage.master" AutoEventWireup="true" CodeFile="COPTCCOPTD.aspx.cs" Inherits="CDS_WebPage_SIGN_COPTCCOPTD" %>
+<%@ Register Assembly="Ede.Uof.Utility.Component.Grid" Namespace="Ede.Uof.Utility.Component" TagPrefix="Fast" %>
+
+
+
+
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+<script>    
+<%--    $(function () {
+        $("#<%= txtDate1.ClientID %>").datepicker({ dateFormat: "yymmdd", });       
+    });--%>
+
+   
+</script>
+     <table class="PopTable" >         
+         <tr>
+            <td class="PopTableLeftTD">
+                <asp:Label ID="Label2" runat="server" Text="日期:" meta:resourcekey="Label1Resource1"></asp:Label>             
+            </td>
+            <td class="PopTableRightTD">                    
+                <asp:TextBox ID="txtDate1"  runat="server" Width = "100px"></asp:TextBox>           
+                <asp:Button ID="Button1" runat="server" Text=" 查詢 "
+                    onclick="btn1_Click" meta:resourcekey="btn1Resource1" />                
+            </td>            
+        </tr>
+    </table>
+    <label>訂單</label>
+
+    <table class="PopTable">
+          <tr>
+            <td colspan="2" class="PopTableRightTD" >
+                <div style="overflow-x:auto;width:100%">
+                    <Fast:Grid ID="Grid1" OnRowDataBound="Grid1_RowDataBound" runat="server"  OnBeforeExport="OnBeforeExport1" AllowPaging="true"  AutoGenerateCheckBoxColumn="False" AllowSorting="True" AutoGenerateColumns="False" CustomDropDownListPage="False" DataKeyOnClientWithCheckBox="False" DefaultSortDirection="Ascending" EmptyDataText="No data found" EnhancePager="True" KeepSelectedRows="False" PageSize="100" SelectedRowColor="" UnSelectedRowColor="" meta:resourcekey="Grid1Resource1" OnPageIndexChanging="grid_PageIndexChanging" >
+                        <EnhancePagerSettings FirstImageUrl="" FirstAltImageUrl="" PreviousImageUrl="" NextImageUrl="" LastImageUrl="" LastAltImage="" PageNumberCssClass="" PageNumberCurrentCssClass="" PageInfoCssClass="" PageRedirectCssClass="" NextIAltImageUrl="" PreviousAltImageUrl="" ShowHeaderPager="True"></EnhancePagerSettings>
+                 
+                            <ExportExcelSettings AllowExportToExcel="true" ExportType="DataSource" ></ExportExcelSettings>
+                            <Columns>
+                                  <asp:TemplateField HeaderText="CheckBox" ItemStyle-Width="100px" HeaderStyle-HorizontalAlign="Center"  ItemStyle-HorizontalAlign="Center">
+                                    <ItemTemplate>
+                                        <asp:CheckBox ID="CheckBox" AutoCallBack="true" runat="server"></asp:CheckBox>
+                                    </ItemTemplate>
+                                  </asp:TemplateField>                               
+                                 <asp:BoundField HeaderText="訂單單別" DataField="TC001" ItemStyle-Width="100px" HeaderStyle-HorizontalAlign="Center"  ItemStyle-HorizontalAlign="Center" >
+                                    <ItemStyle HorizontalAlign="Center" Width="100px"></ItemStyle>
+                                </asp:BoundField>  
+                                 <asp:BoundField HeaderText="訂單單號" DataField="TC002" ItemStyle-Width="200px" HeaderStyle-HorizontalAlign="Center"  ItemStyle-HorizontalAlign="Center" >
+                                    <ItemStyle HorizontalAlign="Center" Width="200px"></ItemStyle>
+                                </asp:BoundField>  
+                                   <asp:BoundField HeaderText="訂單金額" DataField="TC029" ItemStyle-Width="200px" HeaderStyle-HorizontalAlign="Center"  ItemStyle-HorizontalAlign="Center" >
+                                    <ItemStyle HorizontalAlign="Center" Width="200px"></ItemStyle>
+                                </asp:BoundField>  
+                                   <asp:BoundField HeaderText="訂單稅額" DataField="TC030" ItemStyle-Width="200px" HeaderStyle-HorizontalAlign="Center"  ItemStyle-HorizontalAlign="Center" >
+                                    <ItemStyle HorizontalAlign="Center" Width="200px"></ItemStyle>
+                                </asp:BoundField>  
+                                   <asp:BoundField HeaderText="訂單合計" DataField="MONEYS" ItemStyle-Width="200px" HeaderStyle-HorizontalAlign="Center"  ItemStyle-HorizontalAlign="Center" >
+                                    <ItemStyle HorizontalAlign="Center" Width="200px"></ItemStyle>
+                                </asp:BoundField>  
+                                
+                                
+                            </Columns>
+                        </Fast:Grid>
+                    </div>
+            </td>
+        </tr>
+    </table>
+    <table class="PopTable" >         
+         <tr>
+            <td class="PopTableLeftTD">
+                <asp:Label ID="Label1" runat="server" Text="簽核" meta:resourcekey="Label2Resource1"></asp:Label>             
+            </td>
+            <td class="PopTableRightTD">                                    
+                <asp:Button ID="Button2" runat="server" Text=" 簽核 " onclick="btn2_Click" meta:resourcekey="btn2Resource1" Width="200px" BackColor="red" />                
+            </td>            
+        </tr>
+    </table>
+        <table class="PopTable" >         
+         <tr>
+            <td class="PopTableLeftTD">
+                <asp:Label ID="Label3" runat="server" Text="" meta:resourcekey="Label2Resource1"></asp:Label>             
+            </td>          
+        </tr>
+    </table>
+
+</asp:Content>
+
---
+++ WebPage/SIGN/COPTCCOPTD.aspx.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Security.AntiXss;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Ede.Uof.Utility.Data;
+using Ede.Uof.Utility.Page.Common;
+
+public partial class CDS_WebPage_SIGN_COPTCCOPTD : Ede.Uof.Utility.Page.BasePage
+{
+
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            BindGrid(DateTime.Now.ToString("yyyyMMdd"));
+            txtDate1.Text = DateTime.Now.ToString("yyyyMMdd");
+          
+
+        }
+        else
+        {
+            //BindGrid(txtDate1.Text);
+
+        }
+    }
+
+    #region FUNCTION
+    private void BindGrid(string SDATE)
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["ERPconnectionstring"].ToString();
+        Ede.Uof.Utility.Data.DatabaseHelper m_db = new Ede.Uof.Utility.Data.DatabaseHelper(connectionString);
+
+        string cmdTxt = @" 
+                        SELECT TC001,TC002,CONVERT(INT,TC029) TC029,CONVERT(INT,TC030) TC030,CONVERT(INT,(TC029+TC030)) AS MONEYS
+                        FROM [TK].dbo.COPTC
+                        WHERE TC027='N' 
+                        AND  TC003=@SDATE
+                        ORDER BY TC001,TC002
+                        ";
+
+        m_db.AddParameter("@SDATE", SDATE);
+
+        DataTable dt = new DataTable();
+
+        dt.Load(m_db.ExecuteReader(cmdTxt));
+
+        Grid1.DataSource = dt;
+        Grid1.DataBind();
+    }
+
+    protected void grid_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        Grid1.PageIndex = e.NewPageIndex;
+        BindGrid(this.Session["SDATE"].ToString());
+    }
+    protected void Grid1_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+       
+        //    if (e.Row.RowType == DataControlRowType.DataRow)
+        //    {
+        //        DataRowView row = (DataRowView)e.Row.DataItem;
+        //        LinkButton lbtnName = (LinkButton)e.Row.FindControl("lbtnName");
+
+        //        ExpandoObject param = new { ID = row["ID"].ToString() }.ToExpando();
+
+        //        //Grid開窗是用RowDataBound事件再開窗
+        //        Dialog.Open2(lbtnName, "~/CDS/WebPage/TKRESEARCHTBDEVMEMODialogEDITDEL.aspx", "", 800, 600, Dialog.PostBackType.AfterReturn, param);
+        //    }
+
+
+    }
+
+    public void OnBeforeExport1(object sender, Ede.Uof.Utility.Component.BeforeExportEventArgs e)
+    {
+        
+    }
+    public void UPDATECOPTCCOPTD(string SQL)
+    {
+        string TC027 = "Y";
+        string TC048 = "N";
+        string TD021 = "Y";
+        string COMPANY = "TK";
+        string MODIFIER = "160115";
+        string MODI_DATE = DateTime.Now.ToString("yyyyMMdd");
+        string MODI_TIME = DateTime.Now.ToString("HH:mm:dd");
+
+        string connectionString = ConfigurationManager.ConnectionStrings["ERPconnectionstring"].ToString();
+
+        var SQLCOMMAND = SQL;
+
+        try
+        {
+            using (SqlConnection cnn = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(SQLCOMMAND, cnn))
+                {
+
+                    cmd.Parameters.AddWithValue("@TC027", TC027);
+                    cmd.Parameters.AddWithValue("@TC048", TC048);
+                    cmd.Parameters.AddWithValue("@TD021", TD021);
+
+                    cmd.Parameters.AddWithValue("@COMPANY", COMPANY);
+                    cmd.Parameters.AddWithValue("@MODIFIER", MODIFIER);
+                    cmd.Parameters.AddWithValue("@MODI_DATE", MODI_DATE);
+                    cmd.Parameters.AddWithValue("@MODI_TIME", MODI_TIME);
+
+                    cnn.Open();
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+
+                    if (rowsAffected>=1)
+                    {
+                        Label3.Text = "完成";
+                    }
+                }
+            }
+        }
+
+        catch
+        {
+
+        }
+        finally
+        {
+
+        }
+        
+    }
+
+    public string SETSQL(string TC001TC002)
+    {
+        StringBuilder SQL = new StringBuilder();
+
+        SQL.AppendFormat(@"
+                        UPDATE [TK].dbo.COPTC
+                        SET TC027=@TC027,TC048=@TC048, FLAG=FLAG+1,COMPANY=@COMPANY,MODIFIER=@MODIFIER ,MODI_DATE=@MODI_DATE, MODI_TIME=@MODI_TIME 
+                        WHERE TC001+TC002 IN ({0})
+
+                        UPDATE [TK].dbo.COPTD 
+                        SET TD021=@TD021, FLAG=FLAG+1,COMPANY=@COMPANY,MODIFIER=@MODIFIER ,MODI_DATE=@MODI_DATE, MODI_TIME=@MODI_TIME 
+                        WHERE TD001+TD002 IN ({0})
+                        ", TC001TC002);
+
+        return SQL.ToString();
+    }
+
+    #endregion
+
+
+    #region BUTTON
+    protected void btn1_Click(object sender, EventArgs e)
+    {
+        BindGrid(txtDate1.Text);
+    }
+
+    protected void btn2_Click(object sender, EventArgs e)
+    {
+        string TC001TC002 = "";
+
+        foreach (GridViewRow gvr in this.Grid1.Rows)
+        {
+            Control ctl = gvr.FindControl("CheckBox");
+            CheckBox ck = (CheckBox)ctl;
+            if (ck.Checked)
+            {
+                TableCellCollection cell = gvr.Cells;
+                TC001TC002 += "'"+cell[1].Text+ cell[2].Text + "',";
+            }
+        }
+        TC001TC002 += "''";
+        Label3.Text = TC001TC002.ToString();
+
+        string SQL= SETSQL(Label3.Text);
+
+        UPDATECOPTCCOPTD(SQL);
+
+        BindGrid(txtDate1.Text);
+    }
+    #endregion
+}
