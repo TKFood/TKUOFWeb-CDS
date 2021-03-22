a9dd188a53e6a79e3a2b2968b81f8fe3f0c88ed3
TKFood <tk290@tkfood.com.tw>
2021-03-22 10:57:57 +0800

    add  TBBU_PRODUCTS

---
+++ WebPage/COP/TBBU_PRODUCTS.aspx.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Ede.Uof.Utility.Data;
+using Ede.Uof.Utility.Page.Common;
+
+public partial class CDS_WebPage_COP_TBBU_PRODUCTS : Ede.Uof.Utility.Page.BasePage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        if (!IsPostBack)
+        {
+            BindGrid();
+        }
+        else
+        {
+            BindGrid();
+        }
+    }
+    #region FUNCTION
+
+    private void BindGrid()
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["ERPconnectionstring"].ToString();
+        Ede.Uof.Utility.Data.DatabaseHelper m_db = new Ede.Uof.Utility.Data.DatabaseHelper(connectionString);
+
+        string cmdTxt = @" 
+                       SELECT [PRODUCTS].[MB001],[PRODUCTSFEATURES],[SALESFOCUS],[COPYWRITINGS],[PICPATHS]
+                        ,MB002,MB003,MB004,MA003,ISNULL(MD007,0) AS MD007,CONVERT(NVARCHAR,MB023)+(CASE WHEN MB198='1' THEN '天' ELSE (CASE WHEN MB198='2' THEN '月' ELSE '年' END ) END ) AS 'VALIDITYPERIOD',ISNULL(MB047,0) AS MB047,MB013
+                        FROM [TKBUSINESS].[dbo].[PRODUCTS],[TK].dbo.[INVMB] 
+                        LEFT JOIN [TK].dbo.INVMA ON MA001='9' AND MA002=MB115
+                        LEFT JOIN [TK].dbo.BOMMD ON MD001=MB001 AND MD003 LIKE '201%'
+                        WHERE [PRODUCTS].[MB001]=[INVMB].[MB001]
+                        ORDER BY [PRODUCTS].[MB001]
+                        ";
+
+        //m_db.AddParameter("@SDATE", SDATE);
+        //m_db.AddParameter("@EDATE", EDATE);
+
+        DataTable dt = new DataTable();
+
+        dt.Load(m_db.ExecuteReader(cmdTxt));
+
+        Grid1.DataSource = dt;
+        Grid1.DataBind();
+    }
+
+    protected void grid1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        //Grid1.PageIndex = e.NewPageIndex;
+        //BindGrid();
+    }
+    protected void Grid1_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+        //if (e.Row.RowType == DataControlRowType.DataRow)
+        //{   
+        //    //Get the button that raised the event
+        //    Button btn = (Button)e.Row.FindControl("Button1");
+           
+        //    //Get the row that contains this button
+        //    GridViewRow gvr = (GridViewRow)btn.NamingContainer;
+
+        //    //string cellvalue = gvr.Cells[2].Text.Trim();
+        //    string Cellvalue = btn.CommandArgument;
+
+        //    DataRowView row = (DataRowView)e.Row.DataItem;
+        //    Button lbtnName = (Button)e.Row.FindControl("Button1");
+
+        //    ExpandoObject param = new { ID = Cellvalue }.ToExpando();
+
+        //    //Grid開窗是用RowDataBound事件再開窗
+        //    Dialog.Open2(lbtnName, "~/CDS/WebPage/COP/TBBU_COPCONDTIONSDialogEDITDEL.aspx", "", 800, 600, Dialog.PostBackType.AfterReturn, param);
+        //}
+
+
+    }
+
+    public void OnBeforeExport1(object sender, Ede.Uof.Utility.Component.BeforeExportEventArgs e)
+    {
+        //string connectionString = ConfigurationManager.ConnectionStrings["ERPconnectionstring"].ToString();
+        //Ede.Uof.Utility.Data.DatabaseHelper m_db = new Ede.Uof.Utility.Data.DatabaseHelper(connectionString);
+
+
+        //string cmdTxt = @" 
+                      
+        //                ";
+
+
+
+        //DataTable dt = new DataTable();
+
+        //dt.Load(m_db.ExecuteReader(cmdTxt));
+
+        //if (dt.Rows.Count > 0)
+        //{
+        //    dt.Columns[0].Caption = "ID";
+           
+
+        //    e.Datasource = dt;
+        //}
+    }
+    #endregion
+
+    #region BUTTON
+    protected void btn_Click(object sender, EventArgs e)
+    {
+
+
+        //開窗後回傳參數
+        if (!string.IsNullOrEmpty(Dialog.GetReturnValue()))
+        {
+            //txtReturnValue.Text = Dialog.GetReturnValue();
+        }
+
+
+    }
+
+
+    protected void btn1_Click(object sender, EventArgs e)
+    {
+        //this.Session["SDATE"] = txtDate1.Text.Trim();
+        //this.Session["EDATE"] = txtDate2.Text.Trim();
+    }
+    protected void MyButtonClick(object sender, System.EventArgs e)
+    {
+      
+
+    }
+    #endregion
+}
