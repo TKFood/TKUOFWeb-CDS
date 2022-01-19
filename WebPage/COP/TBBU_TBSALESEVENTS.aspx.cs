aa02615d85b111dece250282bc6cd35be14da853
TKFood <tk290@tkfood.com.tw>
2022-01-19 10:28:23 +0800

    add

--- WebPage/COP/TBBU_TBSALESEVENTS.aspx.cs
+++ WebPage/COP/TBBU_TBSALESEVENTS.aspx.cs
@@ -18,7 +18,6 @@ using Ede.Uof.Utility.Page.Common;
 using OfficeOpenXml;
 using OfficeOpenXml.Drawing;
 using OfficeOpenXml.Style;
-using Telerik.Web.UI.Calendar;
 using Image = System.Web.UI.WebControls.Image;
 
 public partial class CDS_WebPage_TBBU_TBSALESEVENTS : Ede.Uof.Utility.Page.BasePage
@@ -61,10 +60,6 @@ public partial class CDS_WebPage_TBBU_TBSALESEVENTS : Ede.Uof.Utility.Page.BaseP
 
     }
     #region FUNCTION
-    protected void RadDatePicker3_SelectedDateChanged(object sender, SelectedDateChangedEventArgs e)
-    {
-        RadDatePicker4.SelectedDate = RadDatePicker3.SelectedDate;
-    }
     private void BindDropDownList1()
     {
         DataTable dt = new DataTable();
