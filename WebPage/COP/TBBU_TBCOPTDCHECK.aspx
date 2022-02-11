d8bfc11adabb554979dd8696d6353e22b08139ee
TKFood <tk290@tkfood.com.tw>
2022-02-11 09:53:27 +0800

    ADD CDS_WebPage_COP_TBBU_TBCOPTDCHECK

---
+++ WebPage/COP/TBBU_TBCOPTDCHECK.aspx
@@ -0,0 +1,96 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master/DefaultMasterPage.master" AutoEventWireup="true" CodeFile="TBBU_TBCOPTDCHECK.aspx.cs" Inherits="CDS_WebPage_COP_TBBU_TBCOPTDCHECK" %>
+
+<%@ Register Assembly="Ede.Uof.Utility.Component.Grid" Namespace="Ede.Uof.Utility.Component" TagPrefix="Fast" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+    <script>    
+        function btn4_Click(sender) {
+            //從前端開始視窗
+            //sender為註冊是由哪個視窗開啟，作為事後要觸發哪個元件的依據
+            //OpenDialogResult為關閉視後會執行的JS Function
+            //參數使用JSON格式傳遞
+            //$uof.dialog.open2("~/CDS/WebPage/COP/TBBU_PRODUCTSDialogADD.aspx", sender, "", 800, 600, OpenDialogResult, {});
+
+            return false;
+
+        }
+
+        //如果有設定回傳值則執行sender Event
+        function OpenDialogResult(returnValue) {
+            if (typeof (returnValue) == "undefined")
+                return false;
+            else
+                return true;
+        }
+
+
+
+    </script>
+
+    <telerik:RadTabStrip ID="RadTabStrip1" runat="server"></telerik:RadTabStrip>
+    <telerik:RadTabStrip ID="RadTabStrip2" runat="server" MultiPageID="RadMultiPage" SelectedIndex="0">
+        <Tabs>
+            <telerik:RadTab Text="訂單協調">
+            </telerik:RadTab>
+            <telerik:RadTab Text="資料">
+            </telerik:RadTab>
+        </Tabs>
+    </telerik:RadTabStrip>
+    <telerik:RadMultiPage ID="RadMultiPage" runat="server" SelectedIndex="0">
+        <telerik:RadPageView ID="RadPageView1" runat="server" Selected="true">
+            <div id="tabs-1">
+                <table class="PopTable">
+                    <tr>
+                        <td class="PopTableLeftTD"></td>
+                        <td>
+                            <asp:DropDownList ID="DropDownList1" runat="server"></asp:DropDownList>
+                        </td>
+
+                    </tr>
+                    <tr>
+                        <td class="PopTableLeftTD"></td>
+                        <td>
+                            <asp:Button ID="Button1" runat="server" Text=" 查詢 " OnClick="btn5_Click"
+                                meta:resourcekey="btn5Resource1" />
+                        </td>
+
+                    </tr>
+                </table>
+                <table class="PopTable">
+                    <tr>
+                        <td colspan="2" class="PopTableRightTD">
+                            <div style="overflow-x: auto; width: 100%">
+                                <Fast:Grid ID="Grid1" Style="overflow-x: auto; width: 100%" OnRowDataBound="Grid1_RowDataBound" runat="server" OnBeforeExport="OnBeforeExport1" AllowPaging="true" AutoGenerateCheckBoxColumn="False" AllowSorting="True" AutoGenerateColumns="False" CustomDropDownListPage="False" DataKeyOnClientWithCheckBox="False" DefaultSortDirection="Ascending" EmptyDataText="No data found" EnhancePager="True" KeepSelectedRows="False" PageSize="1000" SelectedRowColor="" UnSelectedRowColor="" meta:resourcekey="GridResource1" OnPageIndexChanging="grid1_PageIndexChanging">
+                                    <EnhancePagerSettings FirstImageUrl="" FirstAltImageUrl="" PreviousImageUrl="" NextImageUrl="" LastImageUrl="" LastAltImage="" PageNumberCssClass="" PageNumberCurrentCssClass="" PageInfoCssClass="" PageRedirectCssClass="" NextIAltImageUrl="" PreviousAltImageUrl="" ShowHeaderPager="True"></EnhancePagerSettings>
+                                    <ExportExcelSettings AllowExportToExcel="true" ExportType="GridContent"></ExportExcelSettings>
+                                    <Columns>
+                                        <asp:BoundField HeaderText="品號" DataField="MB001" ItemStyle-Width="100px" HeaderStyle-HorizontalAlign="Center" ItemStyle-HorizontalAlign="Center">
+                                            <ItemStyle HorizontalAlign="Center" Width="10px"></ItemStyle>
+                                        </asp:BoundField>
+                                        
+                                    </Columns>
+                                </Fast:Grid>
+                            </div>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </telerik:RadPageView>
+        <telerik:RadPageView ID="RadPageView2" runat="server">
+            <div id="tabs-2">
+                <table class="PopTable">
+                   <%-- <tr>
+                        <td class="PopTableLeftTD">
+                            <asp:Label ID="Label1" runat="server" Text="新增資料" meta:resourcekey="Label4Resource1"></asp:Label>
+                        </td>
+                        <td class="PopTableRightTD">
+                            <asp:Button ID="btn4" runat="server" Text="新增資料" ForeColor="red" OnClientClick="return btn4_Click(this)" meta:resourcekey="btn4Resource1" />
+
+                        </td>
+                    </tr>--%>
+                </table>
+            </div>
+        </telerik:RadPageView>
+    </telerik:RadMultiPage>​
+</asp:Content>
+
---
+++ WebPage/COP/TBBU_TBCOPTDCHECK.aspx.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Ede.Uof.Utility.Data;
+using Ede.Uof.Utility.Page.Common;
+using OfficeOpenXml;
+using OfficeOpenXml.Drawing;
+using OfficeOpenXml.Style;
+
+public partial class CDS_WebPage_COP_TBBU_TBCOPTDCHECK : Ede.Uof.Utility.Page.BasePage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            BindDropDownList();
+            BindGrid("");
+        }
+        else
+        {
+            BindGrid("");
+
+
+        }
+
+       
+
+
+    }
+    #region FUNCTION
+    private void BindDropDownList()
+    {
+        DataTable dt = new DataTable();
+        dt.Columns.Add("SALESFOCUS", typeof(String));
+
+
+        string connectionString = ConfigurationManager.ConnectionStrings["ERPconnectionstring"].ToString();
+        Ede.Uof.Utility.Data.DatabaseHelper m_db = new Ede.Uof.Utility.Data.DatabaseHelper(connectionString);
+
+        string cmdTxt = @" SELECT '未核單' AS 'STATUS' UNION ALL SELECT '已核單' AS 'STATUS' ";
+
+        dt.Load(m_db.ExecuteReader(cmdTxt));
+
+        if (dt.Rows.Count > 0)
+        {
+            DropDownList1.DataSource = dt;
+            DropDownList1.DataTextField = "STATUS";
+            DropDownList1.DataValueField = "STATUS";
+            DropDownList1.DataBind();
+
+        }
+        else
+        {
+
+        }
+
+
+
+    }
+    private void BindGrid(string SALESFOCUS)
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["ERPconnectionstring"].ToString();
+        Ede.Uof.Utility.Data.DatabaseHelper m_db = new Ede.Uof.Utility.Data.DatabaseHelper(connectionString);
+
+        StringBuilder cmdTxt = new StringBuilder();
+        StringBuilder QUERYS = new StringBuilder();
+
+      
+
+        //狀態
+        if (!string.IsNullOrEmpty(DropDownList1.Text))
+        {
+            if (DropDownList1.Text.Equals("未核單"))
+            {
+                QUERYS.AppendFormat(@" ");
+            }
+            else if(!DropDownList1.Text.Equals("已核單"))
+            {
+                QUERYS.AppendFormat(@"  ");
+            }
+        }
+
+       
+        cmdTxt.AppendFormat(@" 
+                               
+                                ", QUERYS.ToString());
+
+       
+
+
+        //m_db.AddParameter("@SDATE", SDATE);
+        //m_db.AddParameter("@EDATE", EDATE);
+
+        DataTable dt = new DataTable();
+
+        dt.Load(m_db.ExecuteReader(cmdTxt.ToString()));
+
+        Grid1.DataSource = dt;
+        Grid1.DataBind();
+    }
+
+    protected void grid1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        //Grid1.PageIndex = e.NewPageIndex;
+        //BindGrid();
+    }
+    protected void Grid1_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+        //if (e.Row.RowType == DataControlRowType.DataRow)
+        //{
+        //    //Get the button that raised the event
+        //    Button btn = (Button)e.Row.FindControl("Button1");
+
+        //    //Get the row that contains this button
+        //    GridViewRow gvr = (GridViewRow)btn.NamingContainer;
+
+        //    //string cellvalue = gvr.Cells[2].Text.Trim();
+        //    string Cellvalue = btn.CommandArgument;
+
+        //    DataRowView row = (DataRowView)e.Row.DataItem;
+        //    Button lbtnName = (Button)e.Row.FindControl("Button1");
+
+        //    ExpandoObject param = new { ID = Cellvalue }.ToExpando();
+
+        //    //Grid開窗是用RowDataBound事件再開窗
+        //    Dialog.Open2(lbtnName, "~/CDS/WebPage/COP/TBBU_PRODUCTSDialogEDITDEL.aspx", "", 800, 600, Dialog.PostBackType.AfterReturn, param);
+        //}
+
+
+
+
+        //StringBuilder PATH = new StringBuilder();
+
+        //System.Web.UI.WebControls.Image img = (System.Web.UI.WebControls.Image)e.Row.FindControl("Image1");
+        //if (e.Row.RowType == DataControlRowType.DataRow)
+        //{
+        //    DataRowView row = (DataRowView)e.Row.DataItem;
+        //    System.Web.UI.WebControls.Image img1 = (System.Web.UI.WebControls.Image)e.Row.FindControl("Image1");
+
+
+
+        //    if (!string.IsNullOrEmpty(row["PHOTO_GUID"].ToString()))
+        //    {
+        //        //img.ImageUrl = "https://eip.tkfood.com.tw/UOF/common/filecenter/v3/handler/downloadhandler.ashx?id=8b2a033b-c301-419b-938d-e6cfedf28b82&path=ALBUM%5C2021%5C03&contentType=image%2Fpng&name=40100010650490.png";
+
+
+        //        //PATH.AppendFormat(@"https://eip.tkfood.com.tw/UOF/common/filecenter/v3/handler/downloadhandler.ashx?id={0}&path=ALBUM%5C2021%5C03&contentType=image%2Fpng&name={1}
+        //        //                ", row["RESIZE_FILE_ID"].ToString(), row["PHOTO_DESC"].ToString());
+
+        //        PATH.AppendFormat(@"https://eip.tkfood.com.tw/UOF/Common/FileCenter/V3/Handler/FileControlHandler.ashx?id={0}
+        //                        ", row["RESIZE_FILE_ID"].ToString());
+        //        img.ImageUrl = PATH.ToString();
+
+        //        //img.ImageUrl  = Request.ApplicationPath + "/Common/FileCenter/ShowImage.aspx?id=" + row["THUMBNAIL_FILE_ID"].ToString();
+
+        //        //img.ImageUrl = string.Format("~/Common/FileCenter/Downloadfile.ashx?id={0}", row["THUMBNAIL_FILE_ID"].ToString());
+
+        //        //e.Row.Cells[0].Text = row["THUMBNAIL_FILE_ID"].ToString();
+        //        ////獲取當前行的圖片路徑
+        //        //string ImgUrl = img.ImageUrl;
+        //        ////給帶圖片的單元格添加點擊事件
+        //        //e.Row.Cells[3].Attributes.Add("onclick", e.Row.Cells[3].ClientID.ToString()
+        //        //    + ".checked=true;CellClick('" + ImgUrl + "')");
+
+        //        //  img.ImageUrl = "https://eip.tkfood.com.tw/BM/upload/note/20200926112527.jpg";
+        //    }
+
+
+        //}
+
+
+    }
+
+    public void OnBeforeExport1(object sender, Ede.Uof.Utility.Component.BeforeExportEventArgs e)
+    {
+        SETEXCEL();
+
+        //string connectionString = ConfigurationManager.ConnectionStrings["ERPconnectionstring"].ToString();
+        //Ede.Uof.Utility.Data.DatabaseHelper m_db = new Ede.Uof.Utility.Data.DatabaseHelper(connectionString);
+
+
+        //string cmdTxt = @" 
+        //               SELECT [PRODUCTS].[MB001],[PRODUCTSFEATURES],[SALESFOCUS],[COPYWRITINGS],[PICPATHS]
+        //                ,MB002,MB003,MB004,MA003,ISNULL(MD007,0) AS MD007,CONVERT(NVARCHAR,MB023)+(CASE WHEN MB198='1' THEN '天' ELSE (CASE WHEN MB198='2' THEN '月' ELSE '年' END ) END ) AS 'VALIDITYPERIOD',CONVERT(decimal(16,3),ISNULL(MB047,0)) AS MB047,MB013
+        //                ,[ALBUM_GUID], [PHOTO_GUID],[PHOTO_DESC],[FILE_ID],[RESIZE_FILE_ID],[THUMBNAIL_FILE_ID]
+        //                FROM [TKBUSINESS].[dbo].[PRODUCTS]
+        //                LEFT JOIN [TK].dbo.[INVMB] ON [PRODUCTS].[MB001]=[INVMB].[MB001]
+        //                LEFT JOIN [TK].dbo.INVMA ON MA001='9' AND MA002=MB115
+        //                LEFT JOIN [TK].dbo.BOMMD ON MD001=[INVMB].[MB001] AND MD003 LIKE '201%'
+        //                LEFT JOIN [192.168.1.223].[UOF].[dbo].[TB_EIP_ALBUM_PHOTO] ON [PHOTO_TOPIC]=[PRODUCTS].[MB001] COLLATE Chinese_Taiwan_Stroke_BIN
+        //                ORDER BY [PRODUCTS].[MB001]
+        //                ";
+
+
+
+        //DataTable dt = new DataTable();
+
+        //dt.Load(m_db.ExecuteReader(cmdTxt));
+
+        //if (dt.Rows.Count > 0)
+        //{
+        //    dt.Columns[0].Caption = "ID";
+
+
+        //    e.Datasource = dt;
+        //}
+    }
+
+    //private void AddImage(ExcelWorksheet oSheet, int rowIndex, int colIndex, string imagePath)
+    //{
+    //    Bitmap image = new Bitmap(imagePath);
+    //    ExcelPicture excelImage = null;
+    //    if (image != null)
+    //    {
+    //        excelImage = oSheet.Drawings.AddPicture("Debopam Pal", image);
+    //        excelImage.From.Column = colIndex;
+    //        excelImage.From.Row = rowIndex;
+    //        excelImage.SetSize(100, 100);
+    //        //2x2 px space for better alignment
+    //        excelImage.From.ColumnOff = Pixel2MTU(2);
+    //        excelImage.From.RowOff = Pixel2MTU(2);
+    //    }
+    //}
+
+    //public int Pixel2MTU(int pixels)
+    //{
+    //    int mtus = pixels * 9525;
+    //    return mtus;
+    //}
+
+    public override void VerifyRenderingInServerForm(Control control) 
+    { 
+
+    }
+
+    public void SETEXCEL()
+    {
+       
+
+    }
+
+    #endregion
+
+    #region BUTTON
+    protected void btn_Click(object sender, EventArgs e)
+    {
+        //開窗後回傳參數
+        if (!string.IsNullOrEmpty(Dialog.GetReturnValue()))
+        {
+            //txtReturnValue.Text = Dialog.GetReturnValue();
+        }
+
+
+    }
+
+
+    protected void btn1_Click(object sender, EventArgs e)
+    {
+        //this.Session["SDATE"] = txtDate1.Text.Trim();
+        //this.Session["EDATE"] = txtDate2.Text.Trim();
+    }
+
+    protected void btn2_Click(object sender, EventArgs e)
+    {
+        SETEXCEL();
+    }
+    protected void btn3_Click(object sender, EventArgs e)
+    {
+        Response.ClearContent();
+        Response.AddHeader("content-disposition", "attachment; filename=test.xls");
+        Response.ContentEncoding = System.Text.Encoding.GetEncoding("big5");
+        HttpContext.Current.Response.Write("<meta http-equiv=Content-Type content=text/html;charset=big5>");
+        HttpContext.Current.Response.Write("<head><meta http-equiv=Content-Type content=text/html;charset=big5></head>");
+        Response.Charset = "big5";
+        Response.ContentType = "application/excel";
+
+
+        System.IO.StringWriter sw = new System.IO.StringWriter();
+        HtmlTextWriter htw = new HtmlTextWriter(sw);
+        Grid1.RenderControl(htw);
+        Response.Write(sw.ToString());
+        Response.End();
+    }
+        protected void MyButtonClick(object sender, System.EventArgs e)
+    {
+      
+
+    }
+
+    protected void btn5_Click(object sender, EventArgs e)
+    {
+        BindGrid("");
+    }
+    #endregion
+}
