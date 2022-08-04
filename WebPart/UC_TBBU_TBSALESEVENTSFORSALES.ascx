870775a327c84cef171d6a9859aaede44606b04c
TKFood <tk290@tkfood.com.tw>
2022-08-04 09:47:07 +0800

    ADD

---
+++ WebPart/UC_TBBU_TBSALESEVENTSFORSALES.ascx
@@ -0,0 +1,1 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="UC_TBBU_TBSALESEVENTSFORSALES.ascx.cs" Inherits="CDS_WebPart_UC_TBBU_TBSALESEVENTSFORSALES" %>
---
+++ WebPart/UC_TBBU_TBSALESEVENTSFORSALES.ascx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Dynamic;
+using System.IO;
+using System.Net;
+using System.Text;
+using Ede.Uof.EIP.SystemInfo;
+using Ede.Uof.Utility.Data;
+using Ede.Uof.Utility.Page.Common;
+using OfficeOpenXml;
+using OfficeOpenXml.Drawing;
+using OfficeOpenXml.Style;
+using Telerik.Web.UI.Calendar;
+using Image = System.Web.UI.WebControls.Image;
+
+
+public partial class CDS_WebPart_UC_TBBU_TBSALESEVENTSFORSALES : System.Web.UI.UserControl
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+}
