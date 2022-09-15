c013e10f524bd24548687ce8e9cc11f61806c6e6
TKFood <tk290@tkfood.com.tw>
2022-09-15 14:24:08 +0800

    CutImage

--- WebPage/Mobile/Mobile_TEST3.aspx.cs
+++ WebPage/Mobile/Mobile_TEST3.aspx.cs
@@ -20,6 +20,8 @@ using OfficeOpenXml;
 using OfficeOpenXml.Drawing;
 using OfficeOpenXml.Style;
 using System.Web.Services;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 
 public partial class CDS_WebPage_Mobile_Mobile_TEST3 : System.Web.UI.Page
 {
@@ -140,23 +142,77 @@ public partial class CDS_WebPage_Mobile_Mobile_TEST3 : System.Web.UI.Page
     [WebMethod()]
     public static string SaveCapturedImage(string myTextcontent,string data)
     {
-        string NOWTIMES = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
-      
+        string NOWTIMES = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");      
         string fileName = DateTime.Now.ToString("yyyyMMddHHmmss");
 
+        string ORI;
+        string COM;
         ////Convert Base64 Encoded string to Byte Array.
         byte[] imageBytes = Convert.FromBase64String(data.Split(',')[1]);
+        ORI = imageBytes.Length.ToString();
 
-        STATICADDTKGAFFAIRSCHECKSPOOINTPHOTO(myTextcontent, NOWTIMES, imageBytes);
+        byte[] imageBytes2 = CutImage(imageBytes,100,100);
+        COM = imageBytes2.Length.ToString();
+        //STATICADDTKGAFFAIRSCHECKSPOOINTPHOTO(myTextcontent, NOWTIMES, imageBytes);
 
         ////Save the Byte Array as Image File.
         //string filePath = HttpContext.Current.Server.MapPath(string.Format("~/Captures/{0}.jpg", fileName));
         //File.WriteAllBytes(filePath, imageBytes);
         //return true;
 
-        string MESSAGE = imageBytes.Length.ToString()+" "+ fileName + " 拍照成功";
+        string MESSAGE = ORI +" / "+ COM + " "+ fileName + " 拍照成功";
         return MESSAGE;
     }
+
+    /// <summary>
+    /// 生成縮略圖
+    /// </summary>
+    /// <param name="srcImageStream">原始圖片的位元組陣列</param>
+    /// <param name="width">最終的圖片寬度</param>
+    /// <param name="height">最終的圖片高度</param>
+    /// <returns>壓縮後的base64字串</returns>
+    public static byte[] CutImage(byte[] srcImageStream, int width, int height)
+    {
+        MemoryStream msSource = new MemoryStream(srcImageStream);
+        Bitmap btImage = new Bitmap(msSource);
+        msSource.Close();
+        System.Drawing.Image serverImage = btImage;
+        //畫板大小
+        int finalWidth = width, finalHeight = height;
+        int srcImageWidth = serverImage.Width;
+        int srcImageHeight = serverImage.Height;
+        if (srcImageWidth > srcImageHeight)
+        {
+            finalHeight = srcImageHeight * width / srcImageWidth;
+        }
+        else
+        {
+            finalWidth = srcImageWidth * height / srcImageHeight;
+        }
+        //新建一個bmp圖片
+        System.Drawing.Image newImage = new Bitmap(width, height);
+        //新建一個畫板
+        Graphics g = Graphics.FromImage(newImage);
+        //設置高品質插值法
+        g.InterpolationMode = InterpolationMode.High;
+        //設置高品質,低速度呈現平滑程度
+        g.SmoothingMode = SmoothingMode.HighQuality;
+        //清空畫布並以透明背景色填充
+        g.Clear(Color.White);
+        //在指定位置並且按指定大小繪製原圖片的指定部分
+        g.DrawImage(serverImage, new Rectangle((width - finalWidth) / 2, (height - finalHeight) / 2, finalWidth, finalHeight), 0, 0, srcImageWidth, srcImageHeight, GraphicsUnit.Pixel);
+        //以jpg格式保存縮略圖
+        MemoryStream msSaveImage = new MemoryStream();
+        newImage.Save(msSaveImage, ImageFormat.Jpeg);
+        serverImage.Dispose();
+        newImage.Dispose();
+        g.Dispose();
+        byte[] imageBytes = msSaveImage.ToArray();
+        msSaveImage.Close();
+
+        return imageBytes;
+    }
+
 
     #endregion
 
@@ -167,7 +223,7 @@ public partial class CDS_WebPage_Mobile_Mobile_TEST3 : System.Web.UI.Page
     /// </summary>
     /// <param name="sender"></param>
     /// <param name="e"></param>
-    protected  void Button1_Click(object sender, EventArgs e)
+    protected void Button1_Click(object sender, EventArgs e)
     {
         string CHECKSPOINT = myTextcontent.Text.ToString();
 
