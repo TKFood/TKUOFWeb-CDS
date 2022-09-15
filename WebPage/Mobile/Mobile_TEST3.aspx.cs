f826c7462c8cb244ac3b11ee8a9f79cf85cab800
TKFood <tk290@tkfood.com.tw>
2022-09-15 15:01:50 +0800

    CutImage

--- WebPage/Mobile/Mobile_TEST3.aspx.cs
+++ WebPage/Mobile/Mobile_TEST3.aspx.cs
@@ -145,23 +145,27 @@ public partial class CDS_WebPage_Mobile_Mobile_TEST3 : System.Web.UI.Page
         string NOWTIMES = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");      
         string fileName = DateTime.Now.ToString("yyyyMMddHHmmss");
 
-        string ORI;
-        string COM;
-        ////Convert Base64 Encoded string to Byte Array.
+        string ORI1="";
+        string ORI2="";
+        string ORI3="";
+        ////Convert Base64 Encoded string to Byte Array.       
         byte[] imageBytes = Convert.FromBase64String(data.Split(',')[1]);
-        ORI = imageBytes.Length.ToString();
+        ORI1 = imageBytes.Length.ToString();
+        //加上浮水印
+        byte[] imageBytes2 = GetWatermarkPic(imageBytes, DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")); 
+        ORI2 = imageBytes2.Length.ToString();
+        //壓縮圖片
+        byte[] imageBytes3 = CutImage(imageBytes2, 50, 50);
+        ORI3 = imageBytes3.Length.ToString();
 
-        byte[] imageBytes2 = CutImage(imageBytes,50, 50);
-        COM = imageBytes2.Length.ToString();
-
-        STATICADDTKGAFFAIRSCHECKSPOOINTPHOTO(myTextcontent, NOWTIMES, imageBytes2);
+        STATICADDTKGAFFAIRSCHECKSPOOINTPHOTO(myTextcontent, NOWTIMES, imageBytes3);
 
         ////Save the Byte Array as Image File.
         //string filePath = HttpContext.Current.Server.MapPath(string.Format("~/Captures/{0}.jpg", fileName));
         //File.WriteAllBytes(filePath, imageBytes);
         //return true;
 
-        string MESSAGE = ORI +" / "+ COM + " "+ fileName + " 拍照成功";
+        string MESSAGE = ORI1 + " / "+ ORI2+" / "+ ORI3 + " "+ fileName + " 拍照成功";
         return MESSAGE;
     }
 
@@ -214,6 +218,38 @@ public partial class CDS_WebPage_Mobile_Mobile_TEST3 : System.Web.UI.Page
         return imageBytes;
     }
 
+    ///給圖片加浮水印
+    public static byte[] GetWatermarkPic(byte[] bytes, string txt)
+    {
+        MemoryStream ms = new MemoryStream(bytes);
+        System.Drawing.Image pic = System.Drawing.Image.FromStream(ms);
+        //創建一把刷子
+        //防止索引圖元格式引發異常的圖元格式
+        Bitmap bmp = new Bitmap(pic.Width, pic.Height, PixelFormat.Format32bppArgb);
+        Graphics grap = Graphics.FromImage(bmp);
+        grap.DrawImage(pic, 0, 0, pic.Width, pic.Height);
+        //計算字體大小
+        int fontSize = 22;
+        if (pic.Width <= txt.Length * fontSize)
+        {
+            while (pic.Width <= (txt.Length * fontSize + 10))
+            {
+                fontSize = fontSize - 2;
+            }
+        }
+        //浮水印寬度
+        int txtWidth = txt.Length * fontSize * 2;
+        //字體
+        var font = new Font("黑體", fontSize);
+        Brush brush = new SolidBrush(Color.FromArgb(120, 193, 143, 8));
+        grap.RotateTransform(0);
+        grap.DrawString(txt, font, brush, float.Parse((pic.Width / 2 - txtWidth * 0.3).ToString()), 0);
+        grap.Dispose();
+        //返回有浮水印的圖片流
+        MemoryStream stream = new MemoryStream();
+        bmp.Save(stream, ImageFormat.Png);
+        return stream.GetBuffer();
+    }
 
     #endregion
 
