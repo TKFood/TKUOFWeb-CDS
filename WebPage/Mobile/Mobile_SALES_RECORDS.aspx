8441b61d8aa6bf22aa816989de65b3d0b4d64da3
TKFood <tk290@tkfood.com.tw>
2023-11-08 09:58:39 +0800

    ADD CDS_WebPage_Mobile_SALES_RECORDS

---
+++ WebPage/Mobile/Mobile_SALES_RECORDS.aspx
@@ -0,0 +1,132 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master/MobileMasterPage.master" AutoEventWireup="true" CodeFile="Mobile_SALES_RECORDS.aspx.cs" Inherits="CDS_WebPage_Mobile_SALES_RECORDS" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+  
+    <html>
+
+    <head>
+        <title>拍照範例</title>
+    </head>
+    <body>
+        <div>
+            <table>
+                <tr>
+                    <td>
+                        <h2>拜訪拍照</h2>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="file" accept="image/*" capture="camera" id="photoInput" style="display: none" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <button type="button" id="takePhotoButton">拍照</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <h2>照片</h2>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <img id="previewImage" style="max-width: 50%;" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="Label1" runat="server" Text="客戶: "></asp:Label>
+                        <asp:TextBox ID="myTextcontent" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <button type="button" id="btnUpload">上傳存檔</button>
+                    </td>
+                </tr>
+            </table>
+
+
+
+        </div>
+    </body>
+
+    
+    <script>
+        window.onload = function () {
+            var photoInput = document.getElementById("photoInput");
+            var takePhotoButton = document.getElementById("takePhotoButton");
+            var previewImage = document.getElementById("previewImage");
+
+            takePhotoButton.onclick = function () {
+                photoInput.click();
+            };
+
+            photoInput.onchange = function () {
+                if (photoInput.files && photoInput.files[0]) {
+                    var reader = new FileReader();
+                    reader.onload = function (e) {
+                        previewImage.src = e.target.result;
+                    };
+                    reader.readAsDataURL(photoInput.files[0]);
+                }
+            };
+        };
+
+        //JS執行成功Success
+        function Success(result) {
+            alert(result);
+        }
+        //JS執行失敗
+        function Failure(error) {
+            alert(error);
+        }
+
+        $(function () {
+            $("#btnUpload").click(function () {
+                var myTextcontent = document.getElementById('<%=myTextcontent.ClientID%>').value;
+                var previewImage = document.getElementById("previewImage");
+                var imgCapture = $("#previewImage")[0].src;
+        
+
+                // 壓縮圖片的函數
+                function compressImage(image, quality, callback) {
+                    const canvas = document.createElement('canvas');
+                    const context = canvas.getContext('2d');
+                    canvas.width = image.width;
+                    canvas.height = image.height;
+                    context.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+                    canvas.toBlob(function (blob) {
+                        callback(blob);
+                    }, 'image/jpeg', quality);
+                }
+
+                // 壓縮圖片並使用 PageMethods.SaveCapturedImage 上傳
+                compressImage(previewImage, 0.5, function (compressedBlob) {
+                    // 將壓縮後的圖片轉換為Base64字串
+                    const reader = new FileReader();
+                    reader.onload = function () {
+                        const compressedBase64 = reader.result;
+
+                        // 使用 PageMethods.SaveCapturedImage 上傳壓縮後的圖片
+                        PageMethods.SaveCapturedImage(myTextcontent, compressedBase64, Success, Failure);
+                    };
+                    reader.readAsDataURL(compressedBlob);
+                });
+
+                //PageMethods.SaveCapturedImage(myTextcontent, imgCapture, Success, Failure);
+                //PageMethods.TEST(Success, Failure);
+
+            });
+        });
+    </script>
+
+    </html>
+
+
+
+</asp:Content>
+
---
+++ WebPage/Mobile/Mobile_SALES_RECORDS.aspx.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Dynamic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Security.AntiXss;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Ede.Uof.Utility.Data;
+using Ede.Uof.Utility.Page.Common;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Text;
+using OfficeOpenXml;
+using OfficeOpenXml.Drawing;
+using OfficeOpenXml.Style;
+using System.Web.Services;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+public partial class CDS_WebPage_Mobile_SALES_RECORDS : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+
+        if (!IsPostBack)
+        {
+
+        }
+    }
+
+    #region FUNCTION
+    public static void STATICADDTKGAFFAIRSCHECKSPOOINTPHOTO(string CHECKSPOINT, string CHECKSTIME, byte[] PHOTOS)
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["ERPconnectionstring"].ToString();
+        Ede.Uof.Utility.Data.DatabaseHelper m_db = new Ede.Uof.Utility.Data.DatabaseHelper(connectionString);
+
+        string cmdTxt = @"  
+                       INSERT INTO [TKGAFFAIRS].[dbo].[CHECKSPOOINTPHOTO]
+                        ([CHECKSPOINT],[CHECKSTIME],[PHOTOS])
+                        VALUES
+                        (@CHECKSPOINT,@CHECKSTIME,@PHOTOS)
+                            ";
+
+
+        m_db.AddParameter("@CHECKSPOINT", CHECKSPOINT);
+        m_db.AddParameter("@CHECKSTIME", CHECKSTIME);
+        m_db.AddParameter("@PHOTOS", PHOTOS);
+
+
+        m_db.ExecuteNonQuery(cmdTxt);
+    }
+
+    [WebMethod()]
+    public static string TEST()
+    {
+        string NOWTIMES = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
+        string MESSAGE = NOWTIMES + " 拍照成功";
+        return MESSAGE;
+    }
+    [WebMethod()]
+    public static string SaveCapturedImage(string myTextcontent, string data)
+    {
+        string NOWTIMES = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
+
+        string ORI1 = "";
+        string ORI2 = "";
+        string ORI3 = "";
+        ////Convert Base64 Encoded string to Byte Array.       
+        byte[] imageBytes = Convert.FromBase64String(data.Split(',')[1]);
+        ORI1 = imageBytes.Length.ToString();
+        //加上浮水印
+        byte[] imageBytes2 = GetWatermarkPic(imageBytes, DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss"));
+        ORI2 = imageBytes2.Length.ToString();
+        ////壓縮圖片
+        //byte[] imageBytes3 = CutImage(imageBytes2, 50, 50);
+        //ORI3 = imageBytes3.Length.ToString();
+
+        STATICADDTKGAFFAIRSCHECKSPOOINTPHOTO(myTextcontent, NOWTIMES, imageBytes2);
+
+        ////Save the Byte Array as Image File.
+        //string filePath = HttpContext.Current.Server.MapPath(string.Format("~/Captures/{0}.jpg", fileName));
+        //File.WriteAllBytes(filePath, imageBytes);
+        //return true;
+
+        string MESSAGE = NOWTIMES + " 拍照成功";
+        return MESSAGE;
+    }
+    /// <summary>
+    /// 生成縮略圖
+    /// </summary>
+    /// <param name="srcImageStream">原始圖片的位元組陣列</param>
+    /// <param name="width">最終的圖片寬度</param>
+    /// <param name="height">最終的圖片高度</param>
+    /// <returns>壓縮後的base64字串</returns>
+    public static byte[] CutImage(byte[] srcImageStream, int width, int height)
+    {
+        MemoryStream msSource = new MemoryStream(srcImageStream);
+        Bitmap btImage = new Bitmap(msSource);
+        msSource.Close();
+        System.Drawing.Image serverImage = btImage;
+        //畫板大小
+        int finalWidth = width, finalHeight = height;
+        int srcImageWidth = serverImage.Width;
+        int srcImageHeight = serverImage.Height;
+        if (srcImageWidth > srcImageHeight)
+        {
+            finalHeight = srcImageHeight * width / srcImageWidth;
+        }
+        else
+        {
+            finalWidth = srcImageWidth * height / srcImageHeight;
+        }
+        //新建一個bmp圖片
+        System.Drawing.Image newImage = new Bitmap(width, height);
+        //新建一個畫板
+        Graphics g = Graphics.FromImage(newImage);
+        //設置高品質插值法
+        g.InterpolationMode = InterpolationMode.High;
+        //設置高品質,低速度呈現平滑程度
+        g.SmoothingMode = SmoothingMode.HighQuality;
+        //清空畫布並以透明背景色填充
+        g.Clear(Color.White);
+        //在指定位置並且按指定大小繪製原圖片的指定部分
+        g.DrawImage(serverImage, new Rectangle((width - finalWidth) / 2, (height - finalHeight) / 2, finalWidth, finalHeight), 0, 0, srcImageWidth, srcImageHeight, GraphicsUnit.Pixel);
+        //以jpg格式保存縮略圖
+        MemoryStream msSaveImage = new MemoryStream();
+        newImage.Save(msSaveImage, ImageFormat.Jpeg);
+        serverImage.Dispose();
+        newImage.Dispose();
+        g.Dispose();
+        byte[] imageBytes = msSaveImage.ToArray();
+        msSaveImage.Close();
+
+        return imageBytes;
+    }
+
+    ///給圖片加浮水印
+    public static byte[] GetWatermarkPic(byte[] bytes, string txt)
+    {
+        MemoryStream ms = new MemoryStream(bytes);
+        System.Drawing.Image pic = System.Drawing.Image.FromStream(ms);
+        //創建一把刷子
+        //防止索引圖元格式引發異常的圖元格式
+        Bitmap bmp = new Bitmap(pic.Width, pic.Height, PixelFormat.Format32bppArgb);
+        Graphics grap = Graphics.FromImage(bmp);
+        grap.DrawImage(pic, 0, 0, pic.Width, pic.Height);
+        //計算字體大小
+        int fontSize = 40;
+        if (pic.Width <= txt.Length * fontSize)
+        {
+            while (pic.Width <= (txt.Length * fontSize + 10))
+            {
+                fontSize = fontSize - 2;
+            }
+        }
+        //浮水印寬度
+        int txtWidth = txt.Length * fontSize;
+        //字體
+        var font = new Font("黑體", fontSize);
+        Brush brush = new SolidBrush(Color.FromArgb(120, 193, 143, 8));
+        grap.RotateTransform(0);
+        grap.DrawString(txt, font, brush, float.Parse((pic.Width / 2 - txtWidth * 0.3).ToString()), 0);
+        grap.Dispose();
+        //返回有浮水印的圖片流
+        MemoryStream stream = new MemoryStream();
+        bmp.Save(stream, ImageFormat.Png);
+        return stream.GetBuffer();
+    }
+
+
+    #endregion
+
+
+    #region BUTTON
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    #endregion
+
+
+}
