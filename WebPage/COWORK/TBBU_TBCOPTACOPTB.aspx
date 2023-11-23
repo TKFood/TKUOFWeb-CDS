2b17259dd8675c3c9a024b38086dc282f743aeda
TKFood <tk290@tkfood.com.tw>
2023-11-23 11:05:08 +0800

    add CDS_WebPage_COWORK_TBBU_TBCOPTACOPTB

---
+++ WebPage/COWORK/TBBU_TBCOPTACOPTB.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master/DefaultMasterPage.master" AutoEventWireup="true" CodeFile="TBBU_TBCOPTACOPTB.aspx.cs" Inherits="CDS_WebPage_COWORK_TBBU_TBCOPTACOPTB" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+</asp:Content>
+
---
+++ WebPage/COWORK/TBBU_TBCOPTACOPTB.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.Linq;
+using Ede.Uof.EIP.SystemInfo;
+using Ede.Uof.Utility.Data;
+using Ede.Uof.Utility.Log;
+using Ede.Uof.Utility.Page.Common;
+using OfficeOpenXml;
+using OfficeOpenXml.Drawing;
+using OfficeOpenXml.Style;
+
+public partial class CDS_WebPage_COWORK_TBBU_TBCOPTACOPTB : Ede.Uof.Utility.Page.BasePage
+{
+    string ACCOUNT = null;
+    string NAME = null;
+    String ROLES = null;
+
+    string DBNAME = "UOF";
+    //string DBNAME = "UOFTEST";
+
+    string TA001 = "";
+    string TA002 = "";
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        ACCOUNT = Current.Account;
+        NAME = Current.User.Name;
+
+        if (!IsPostBack)
+        {
+        }
+    }
+
+    #region FUNCTION
+
+    #endregion
+
+    #region BUTTON
+
+    #endregion
+}
