d975cb2c5677a8b087c51684fa87328de24e31e4
TKFood <tk290@tkfood.com.tw>
2023-12-08 16:45:36 +0800

    CDS_WebPart_UC_Mobile_SALES_RECORDS

---
+++ WebPart/UC_Mobile_SALES_RECORDS.ascx
@@ -0,0 +1,1 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="UC_Mobile_SALES_RECORDS.ascx.cs" Inherits="CDS_WebPart_UC_Mobile_SALES_RECORDS" %>
---
+++ WebPart/UC_Mobile_SALES_RECORDS.ascx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class CDS_WebPart_UC_Mobile_SALES_RECORDS : System.Web.UI.UserControl
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+}
