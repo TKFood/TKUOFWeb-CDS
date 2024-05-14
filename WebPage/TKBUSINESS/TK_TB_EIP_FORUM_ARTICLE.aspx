1fa981e910fc32e546b237a399d2c785b0724007
TKFood <tk290@tkfood.com.tw>
2024-05-14 11:03:31 +0800

    add CDS_WebPage_TKBUSINESS_TK_TB_EIP_FORUM_ARTICLE

---
+++ WebPage/TKBUSINESS/TK_TB_EIP_FORUM_ARTICLE.aspx
@@ -0,0 +1,81 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master/DefaultMasterPage.master" AutoEventWireup="true" CodeFile="TK_TB_EIP_FORUM_ARTICLE.aspx.cs" Inherits="CDS_WebPage_TKBUSINESS_TK_TB_EIP_FORUM_ARTICLE" %>
+
+<%@ Register Assembly="Ede.Uof.Utility.Component.Grid" Namespace="Ede.Uof.Utility.Component" TagPrefix="Fast" %>
+
+
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+    <script>          
+        $(function () {
+
+        });
+        //如果有設定回傳值則執行sender Event
+        function OpenDialogResult(returnValue) {
+            if (typeof (returnValue) == "undefined")
+                return false;
+            else
+                return true;
+        }
+    </script>
+
+    <telerik:RadTabStrip ID="RadTabStrip1" runat="server"></telerik:RadTabStrip>
+    <telerik:RadTabStrip ID="RadTabStrip2" runat="server" MultiPageID="RadMultiPage" SelectedIndex="0">
+        <Tabs>
+            <telerik:RadTab Text="報表">
+            </telerik:RadTab>
+            <telerik:RadTab Text="資料">
+            </telerik:RadTab>
+        </Tabs>
+    </telerik:RadTabStrip>
+    <telerik:RadMultiPage ID="RadMultiPage" runat="server" SelectedIndex="0">
+        <telerik:RadPageView ID="RadPageView1" runat="server">
+            <div id="tabs-1">
+                <table class="PopTable">
+                    <tr>
+                        <td>
+                            <asp:Label ID="Label14" runat="server" Text="查詢年度: "></asp:Label>
+                        </td>
+                        <td>
+                            <asp:DropDownList ID="DropDownListISCLOSE" runat="server" AutoPostBack="true" Style="width: 200px;"></asp:DropDownList>
+
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="PopTableLeftTD"></td>
+                        <td>
+                            <asp:Button ID="Button1" runat="server" Text=" 查詢 " OnClick="btn1_Click" meta:resourcekey="btn1_Resource1" />
+                        </td>
+
+                    </tr>
+
+                </table>
+                <table class="PopTable">
+                    <tr>
+                        <td colspan="2" class="PopTableRightTD">
+                            <div style="overflow-x: auto; width: 100%">
+                                <Fast:Grid ID="Grid1" OnRowDataBound="Grid1_RowDataBound" OnRowCommand="Grid1_OnRowCommand" runat="server" OnBeforeExport="OnBeforeExport1" AllowPaging="true" AutoGenerateCheckBoxColumn="False" AllowSorting="True" AutoGenerateColumns="False" CustomDropDownListPage="False" DataKeyOnClientWithCheckBox="False" DefaultSortDirection="Ascending" EmptyDataText="No data found" EnhancePager="True" KeepSelectedRows="False" PageSize="100" SelectedRowColor="" UnSelectedRowColor="" meta:resourcekey="Grid1Resource1" OnPageIndexChanging="grid_PageIndexChanging1">
+                                    <EnhancePagerSettings FirstImageUrl="" FirstAltImageUrl="" PreviousImageUrl="" NextImageUrl="" LastImageUrl="" LastAltImage="" PageNumberCssClass="" PageNumberCurrentCssClass="" PageInfoCssClass="" PageRedirectCssClass="" NextIAltImageUrl="" PreviousAltImageUrl="" ShowHeaderPager="True"></EnhancePagerSettings>
+                                    <ExportExcelSettings AllowExportToExcel="true" ExportType="DataSource"></ExportExcelSettings>
+                                    <Columns>
+                                        <asp:TemplateField HeaderText="討論區" ItemStyle-Width="10%" HeaderStyle-HorizontalAlign="Center" ItemStyle-HorizontalAlign="Left">
+                                            <ItemTemplate>
+                                                <asp:Label ID="討論區" runat="server" Text='<%# Bind("BOARD_NAME") %>' Style="word-break: break-all; white-space: pre-line;"></asp:Label>
+                                            </ItemTemplate>
+                                        </asp:TemplateField>
+                                      
+                                    </Columns>
+                                </Fast:Grid>
+                            </div>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </telerik:RadPageView>
+
+        <telerik:RadPageView ID="RadPageView2" runat="server">
+            <div id="tabs-3">
+            </div>
+        </telerik:RadPageView>
+    </telerik:RadMultiPage>​
+</asp:Content>
+
---
+++ WebPage/TKBUSINESS/TK_TB_EIP_FORUM_ARTICLE.aspx.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Ede.Uof.Utility.Data;
+using Ede.Uof.Utility.Page.Common;
+using OfficeOpenXml;
+using OfficeOpenXml.Drawing;
+using OfficeOpenXml.Style;
+using System.Web.UI.HtmlControls;
+using Ede.Uof.EIP.SystemInfo;
+
+public partial class CDS_WebPage_TKBUSINESS_TK_TB_EIP_FORUM_ARTICLE : Ede.Uof.Utility.Page.BasePage
+{
+   
+    string ACCOUNT = null;
+    string NAME = null;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        ACCOUNT = Current.Account;
+        NAME = Current.User.Name;
+
+        
+
+        if (!IsPostBack)
+        {
+            BindGrid();
+        }
+        else
+        {
+
+        }
+
+
+
+
+    }
+    #region FUNCTION
+   
+
+    private void BindGrid()
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["connectionstring"].ToString();
+        Ede.Uof.Utility.Data.DatabaseHelper m_db = new Ede.Uof.Utility.Data.DatabaseHelper(connectionString);
+
+        StringBuilder cmdTxt = new StringBuilder();
+        StringBuilder Query1 = new StringBuilder();
+
+        cmdTxt.AppendFormat(@" 
+                           --- 20240514 找討論區文章
+
+SELECT TB_EIP_FORUM_BOARD.BOARD_NAME
+,CONVERT(NVARCHAR,TB_EIP_FORUM_TOPIC.CREATE_DATE,112) CREATE_DATE
+,TB_EIP_FORUM_ARTICLE.SUBJECT
+,ISNULL((
+SELECT TOP 1
+[NAME]+':'+CHAR(13)+CHAR(10)+CONVERT(NVARCHAR,[CREATE_DATE],112)+CHAR(13)+CHAR(10)++REPLACE(dbo.udf_StripHTML([cleaned_img_content]),'&nbsp;','')
+FROM [UOF].[dbo].[View_SUB_TB_EIP_FORUM_ARTICLE]
+WHERE [View_SUB_TB_EIP_FORUM_ARTICLE].[SUBJECT]=TB_EIP_FORUM_ARTICLE.SUBJECT
+AND [View_SUB_TB_EIP_FORUM_ARTICLE].[GROUP_NAME] IN (SELECT [DEPNAMES] FROM [UOF].[dbo].[Z_UOF_FORUM_ARTICLE_DEP] WHERE [DEPKINDS] IN ('設計'))
+ORDER BY [View_SUB_TB_EIP_FORUM_ARTICLE].[FLOORS] DESC
+),'') AS '設計'
+,ISNULL((
+SELECT TOP 1
+[NAME]+':'+CHAR(13)+CHAR(10)+CONVERT(NVARCHAR,[CREATE_DATE],112)+CHAR(13)+CHAR(10)++REPLACE(dbo.udf_StripHTML([cleaned_img_content]),'&nbsp;','')
+FROM [UOF].[dbo].[View_SUB_TB_EIP_FORUM_ARTICLE]
+WHERE [View_SUB_TB_EIP_FORUM_ARTICLE].[SUBJECT]=TB_EIP_FORUM_ARTICLE.SUBJECT
+AND [View_SUB_TB_EIP_FORUM_ARTICLE].[GROUP_NAME] IN (SELECT [DEPNAMES] FROM [UOF].[dbo].[Z_UOF_FORUM_ARTICLE_DEP] WHERE [DEPKINDS] IN ('財務'))
+ORDER BY [View_SUB_TB_EIP_FORUM_ARTICLE].[FLOORS] DESC
+),'') AS '財務'
+,ISNULL((
+SELECT TOP 1
+[NAME]+':'+CHAR(13)+CHAR(10)+CONVERT(NVARCHAR,[CREATE_DATE],112)+CHAR(13)+CHAR(10)++REPLACE(dbo.udf_StripHTML([cleaned_img_content]),'&nbsp;','')
+FROM [UOF].[dbo].[View_SUB_TB_EIP_FORUM_ARTICLE]
+WHERE [View_SUB_TB_EIP_FORUM_ARTICLE].[SUBJECT]=TB_EIP_FORUM_ARTICLE.SUBJECT
+AND [View_SUB_TB_EIP_FORUM_ARTICLE].[GROUP_NAME] IN (SELECT [DEPNAMES] FROM [UOF].[dbo].[Z_UOF_FORUM_ARTICLE_DEP] WHERE [DEPKINDS] IN ('生管'))
+ORDER BY [View_SUB_TB_EIP_FORUM_ARTICLE].[FLOORS] DESC
+),'') AS '生管'
+,ISNULL((
+SELECT TOP 1
+[NAME]+':'+CHAR(13)+CHAR(10)+CONVERT(NVARCHAR,[CREATE_DATE],112)+CHAR(13)+CHAR(10)++REPLACE(dbo.udf_StripHTML([cleaned_img_content]),'&nbsp;','')
+FROM [UOF].[dbo].[View_SUB_TB_EIP_FORUM_ARTICLE]
+WHERE [View_SUB_TB_EIP_FORUM_ARTICLE].[SUBJECT]=TB_EIP_FORUM_ARTICLE.SUBJECT
+AND [View_SUB_TB_EIP_FORUM_ARTICLE].[GROUP_NAME] IN (SELECT [DEPNAMES] FROM [UOF].[dbo].[Z_UOF_FORUM_ARTICLE_DEP] WHERE [DEPKINDS] IN ('資材'))
+ORDER BY [View_SUB_TB_EIP_FORUM_ARTICLE].[FLOORS] DESC
+),'') AS '資材'
+,ISNULL((
+SELECT TOP 1
+[NAME]+':'+CHAR(13)+CHAR(10)+CONVERT(NVARCHAR,[CREATE_DATE],112)+CHAR(13)+CHAR(10)++REPLACE(dbo.udf_StripHTML([cleaned_img_content]),'&nbsp;','')
+FROM [UOF].[dbo].[View_SUB_TB_EIP_FORUM_ARTICLE]
+WHERE [View_SUB_TB_EIP_FORUM_ARTICLE].[SUBJECT]=TB_EIP_FORUM_ARTICLE.SUBJECT
+AND [View_SUB_TB_EIP_FORUM_ARTICLE].[GROUP_NAME] IN (SELECT [DEPNAMES] FROM [UOF].[dbo].[Z_UOF_FORUM_ARTICLE_DEP] WHERE [DEPKINDS] IN ('品保'))
+ORDER BY [View_SUB_TB_EIP_FORUM_ARTICLE].[FLOORS] DESC
+),'') AS '品保'
+,ISNULL((
+SELECT TOP 1
+[NAME]+':'+CHAR(13)+CHAR(10)+CONVERT(NVARCHAR,[CREATE_DATE],112)+CHAR(13)+CHAR(10)++REPLACE(dbo.udf_StripHTML([cleaned_img_content]),'&nbsp;','')
+FROM [UOF].[dbo].[View_SUB_TB_EIP_FORUM_ARTICLE]
+WHERE [View_SUB_TB_EIP_FORUM_ARTICLE].[SUBJECT]=TB_EIP_FORUM_ARTICLE.SUBJECT
+AND [View_SUB_TB_EIP_FORUM_ARTICLE].[GROUP_NAME] IN (SELECT [DEPNAMES] FROM [UOF].[dbo].[Z_UOF_FORUM_ARTICLE_DEP] WHERE [DEPKINDS] IN ('研發'))
+ORDER BY [View_SUB_TB_EIP_FORUM_ARTICLE].[FLOORS] DESC
+),'') AS '研發'
+,ISNULL((
+SELECT TOP 1
+[NAME]+':'+CHAR(13)+CHAR(10)+CONVERT(NVARCHAR,[CREATE_DATE],112)+CHAR(13)+CHAR(10)++REPLACE(dbo.udf_StripHTML([cleaned_img_content]),'&nbsp;','')
+FROM [UOF].[dbo].[View_SUB_TB_EIP_FORUM_ARTICLE]
+WHERE [View_SUB_TB_EIP_FORUM_ARTICLE].[SUBJECT]=TB_EIP_FORUM_ARTICLE.SUBJECT
+AND [View_SUB_TB_EIP_FORUM_ARTICLE].[GROUP_NAME] IN (SELECT [DEPNAMES] FROM [UOF].[dbo].[Z_UOF_FORUM_ARTICLE_DEP] WHERE [DEPKINDS] IN ('硯微墨'))
+ORDER BY [View_SUB_TB_EIP_FORUM_ARTICLE].[FLOORS] DESC
+),'') AS '硯微墨'
+,ISNULL((
+SELECT TOP 1
+[NAME]+':'+CHAR(13)+CHAR(10)+CONVERT(NVARCHAR,[CREATE_DATE],112)+CHAR(13)+CHAR(10)++REPLACE(dbo.udf_StripHTML([cleaned_img_content]),'&nbsp;','')
+FROM [UOF].[dbo].[View_SUB_TB_EIP_FORUM_ARTICLE]
+WHERE [View_SUB_TB_EIP_FORUM_ARTICLE].[SUBJECT]=TB_EIP_FORUM_ARTICLE.SUBJECT
+AND [View_SUB_TB_EIP_FORUM_ARTICLE].[GROUP_NAME] IN (SELECT [DEPNAMES] FROM [UOF].[dbo].[Z_UOF_FORUM_ARTICLE_DEP] WHERE [DEPKINDS] IN ('門市'))
+ORDER BY [View_SUB_TB_EIP_FORUM_ARTICLE].[FLOORS] DESC
+),'') AS '門市'
+,ISNULL((
+SELECT TOP 1
+[NAME]+':'+CHAR(13)+CHAR(10)+CONVERT(NVARCHAR,[CREATE_DATE],112)+CHAR(13)+CHAR(10)++REPLACE(dbo.udf_StripHTML([cleaned_img_content]),'&nbsp;','')
+FROM [UOF].[dbo].[View_SUB_TB_EIP_FORUM_ARTICLE]
+WHERE [View_SUB_TB_EIP_FORUM_ARTICLE].[SUBJECT]=TB_EIP_FORUM_ARTICLE.SUBJECT
+AND [View_SUB_TB_EIP_FORUM_ARTICLE].[GROUP_NAME] IN (SELECT [DEPNAMES] FROM [UOF].[dbo].[Z_UOF_FORUM_ARTICLE_DEP] WHERE [DEPKINDS] IN ('觀光'))
+ORDER BY [View_SUB_TB_EIP_FORUM_ARTICLE].[FLOORS] DESC
+),'') AS '觀光'
+,ISNULL((
+SELECT TOP 1
+[NAME]+':'+CHAR(13)+CHAR(10)+CONVERT(NVARCHAR,[CREATE_DATE],112)+CHAR(13)+CHAR(10)++REPLACE(dbo.udf_StripHTML([cleaned_img_content]),'&nbsp;','')
+FROM [UOF].[dbo].[View_SUB_TB_EIP_FORUM_ARTICLE]
+WHERE [View_SUB_TB_EIP_FORUM_ARTICLE].[SUBJECT]=TB_EIP_FORUM_ARTICLE.SUBJECT
+AND [View_SUB_TB_EIP_FORUM_ARTICLE].[GROUP_NAME] IN (SELECT [DEPNAMES] FROM [UOF].[dbo].[Z_UOF_FORUM_ARTICLE_DEP] WHERE [DEPKINDS] IN ('國外'))
+ORDER BY [View_SUB_TB_EIP_FORUM_ARTICLE].[FLOORS] DESC
+),'') AS '國外'
+,ISNULL((
+SELECT TOP 1
+[NAME]+':'+CHAR(13)+CHAR(10)+CONVERT(NVARCHAR,[CREATE_DATE],112)+CHAR(13)+CHAR(10)++REPLACE(dbo.udf_StripHTML([cleaned_img_content]),'&nbsp;','')
+FROM [UOF].[dbo].[View_SUB_TB_EIP_FORUM_ARTICLE]
+WHERE [View_SUB_TB_EIP_FORUM_ARTICLE].[SUBJECT]=TB_EIP_FORUM_ARTICLE.SUBJECT
+AND [View_SUB_TB_EIP_FORUM_ARTICLE].[GROUP_NAME] IN (SELECT [DEPNAMES] FROM [UOF].[dbo].[Z_UOF_FORUM_ARTICLE_DEP] WHERE [DEPKINDS] IN ('國內'))
+ORDER BY [View_SUB_TB_EIP_FORUM_ARTICLE].[FLOORS] DESC
+),'') AS '國內'
+,ISNULL((
+SELECT TOP 1
+[NAME]+':'+CHAR(13)+CHAR(10)+CONVERT(NVARCHAR,[CREATE_DATE],112)+CHAR(13)+CHAR(10)++REPLACE(dbo.udf_StripHTML([cleaned_img_content]),'&nbsp;','')
+FROM [UOF].[dbo].[View_SUB_TB_EIP_FORUM_ARTICLE]
+WHERE [View_SUB_TB_EIP_FORUM_ARTICLE].[SUBJECT]=TB_EIP_FORUM_ARTICLE.SUBJECT
+AND [View_SUB_TB_EIP_FORUM_ARTICLE].[GROUP_NAME] IN (SELECT [DEPNAMES] FROM [UOF].[dbo].[Z_UOF_FORUM_ARTICLE_DEP] WHERE [DEPKINDS] IN ('新事業發展'))
+ORDER BY [View_SUB_TB_EIP_FORUM_ARTICLE].[FLOORS] DESC
+),'') AS '新事業發展'
+
+FROM [UOF].dbo.TB_EIP_FORUM_AREA
+,[UOF].dbo.TB_EIP_FORUM_BOARD
+,[UOF].dbo.TB_EIP_FORUM_TOPIC
+,[UOF].dbo.TB_EIP_FORUM_ARTICLE
+,[UOF].[dbo].[TB_EB_USER]
+,[UOF].[dbo].[TB_EB_EMPL_DEP]
+,[UOF].[dbo].[TB_EB_GROUP]
+WHERE TB_EIP_FORUM_AREA.AREA_GUID=TB_EIP_FORUM_BOARD.AREA_GUID
+AND TB_EIP_FORUM_BOARD.BOARD_GUID=TB_EIP_FORUM_TOPIC.BOARD_GUID
+AND TB_EIP_FORUM_ARTICLE.TOPIC_GUID=TB_EIP_FORUM_TOPIC.TOPIC_GUID
+AND TB_EB_USER.USER_GUID=TB_EIP_FORUM_ARTICLE.ANNOUNCER
+AND TB_EB_EMPL_DEP.USER_GUID=TB_EB_USER.USER_GUID AND TB_EB_EMPL_DEP.ORDERS='0'
+AND TB_EB_GROUP.GROUP_ID=TB_EB_EMPL_DEP.GROUP_ID
+--AND TB_EIP_FORUM_ARTICLE.SUBJECT='20240425-01 2024秋暮追月中秋禮盒(門市款)'
+AND (TB_EIP_FORUM_BOARD.BOARD_NAME LIKE '%校稿%' OR TB_EIP_FORUM_BOARD.BOARD_NAME LIKE '%設計%' )
+AND  CONVERT(NVARCHAR,TB_EIP_FORUM_TOPIC.CREATE_DATE,112)>='2024/1/1'
+--AND TB_EIP_FORUM_ARTICLE.SUBJECT='20240425-01 2024秋暮追月中秋禮盒(門市款)'
+GROUP BY TB_EIP_FORUM_BOARD.BOARD_NAME,CONVERT(NVARCHAR,TB_EIP_FORUM_TOPIC.CREATE_DATE,112),TB_EIP_FORUM_ARTICLE.SUBJECT
+ORDER BY TB_EIP_FORUM_BOARD.BOARD_NAME,CONVERT(NVARCHAR,TB_EIP_FORUM_TOPIC.CREATE_DATE,112),TB_EIP_FORUM_ARTICLE.SUBJECT
+
+                              
+                            ", Query1.ToString());
+
+
+        //m_db.AddParameter("@EDATE", EDATE);
+
+        DataTable dt = new DataTable();
+
+        dt.Load(m_db.ExecuteReader(cmdTxt.ToString()));
+
+        Grid1.DataSource = dt;
+        Grid1.DataBind();
+    }
+
+    protected void grid_PageIndexChanging1(object sender, GridViewPageEventArgs e)
+    {
+        //Grid1.PageIndex = e.NewPageIndex;
+        //BindGrid("");
+    }
+    protected void Grid1_RowDataBound(object sender, GridViewRowEventArgs e)
+    {   
+       
+    }
+
+    protected void Grid1_OnRowCommand(object sender, GridViewCommandEventArgs e)
+    {
+        int rowIndex = -1;
+
+    }
+
+    public void OnBeforeExport1(object sender, Ede.Uof.Utility.Component.BeforeExportEventArgs e)
+    {
+        SETEXCEL();
+
+
+    }
+
+
+    public override void VerifyRenderingInServerForm(Control control)
+    {
+
+    }
+
+    public void SETEXCEL()
+    {
+        
+
+    }
+
+    public void MsgBox(String ex, Page pg, Object obj)
+    {
+        string s = "<SCRIPT language='javascript'>alert('" + ex.Replace("\r\n", "\\n").Replace("'", "") + "'); </SCRIPT>";
+        Type cstype = obj.GetType();
+        ClientScriptManager cs = pg.ClientScript;
+        cs.RegisterClientScriptBlock(cstype, s, s.ToString());
+    }
+
+   
+    #endregion
+
+    #region BUTTON
+    protected void btn_Click(object sender, EventArgs e)
+    {
+
+
+        //開窗後回傳參數
+        if (!string.IsNullOrEmpty(Dialog.GetReturnValue()))
+        {
+            //txtReturnValue.Text = Dialog.GetReturnValue();
+        }
+
+    }
+
+
+    protected void btn1_Click(object sender, EventArgs e)
+    {
+        BindGrid();
+    }
+
+
+
+    #endregion
+}
