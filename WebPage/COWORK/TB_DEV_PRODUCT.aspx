2ff23bbbca49899bd31eb8444433ab0efebf2a23
TKFood <tk290@tkfood.com.tw>
2025-05-07 15:25:21 +0800

    DEV_PRODUCT.aspx

---
+++ WebPage/COWORK/TB_DEV_PRODUCT.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master/DefaultMasterPage.master" AutoEventWireup="true" CodeFile="TB_DEV_PRODUCT.aspx.cs" Inherits="CDS_WebPage_COWORK_TB_DEV_PRODUCT" %>
+
+<%@ Register Assembly="Ede.Uof.Utility.Component.Grid" Namespace="Ede.Uof.Utility.Component" TagPrefix="Fast" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+       <script>    
+
+
+</script>
+    <style>
+        .big-bold-button {
+            font-size: 20px;
+            font-weight: bold;
+            background-color: #007bff;
+            color: white;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 8px;
+            box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.2);
+            cursor: pointer;
+        }
+
+            .big-bold-button:hover {
+                background-color: #0056b3;
+            }
+    </style>
+
+</asp:Content>
+
---
+++ WebPage/COWORK/TB_DEV_PRODUCT.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.Linq;
+using Ede.Uof.EIP.SystemInfo;
+using Ede.Uof.Utility.Data;
+using Ede.Uof.Utility.Log;
+using Ede.Uof.Utility.Page.Common;
+using OfficeOpenXml;
+using OfficeOpenXml.Drawing;
+using OfficeOpenXml.Style;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+public partial class CDS_WebPage_COWORK_TB_DEV_PRODUCT : Ede.Uof.Utility.Page.BasePage
+{
+    string ACCOUNT = null;
+    string NAME = null;
+    string ROLES = null;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        if (!IsPostBack)
+        {
+           
+        }
+    }
+
+    #region FUNCTION
+
+    #endregion
+
+    #region BUTTON
+
+    #endregion
+}
