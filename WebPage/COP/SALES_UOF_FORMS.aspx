8acb5334eb42b2f2ce14897cd9ef4ae4e1f4f781
TKFood <tk290@tkfood.com.tw>
2025-09-03 17:11:15 +0800

    CDS_WebPage_COP_SALES_UOF_FORMS

---
+++ WebPage/COP/SALES_UOF_FORMS.aspx
@@ -0,0 +1,106 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master/DefaultMasterPage.master" AutoEventWireup="true" CodeFile="SALES_UOF_FORMS.aspx.cs" Inherits="CDS_WebPage_COP_SALES_UOF_FORMS" %>
+
+<%@ Register Assembly="Ede.Uof.Utility.Component.Grid" Namespace="Ede.Uof.Utility.Component" TagPrefix="Fast" %>
+
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+    <style>
+        .multiline-textbox {
+            word-break: break-all;
+            white-space: pre-line;
+            height: 60px; /* 根据需要设置高度 */
+        }
+
+        .custom-button {
+            width: 150px; /* 设置按钮宽度 */
+            height: 40px; /* 设置按钮高度 */
+            font-size: 20px; /* 设置按钮字体大小 */
+            color: #ffffff; /* 设置按钮文本颜色 */
+            background-color: #007bff; /* 设置按钮背景颜色 */
+            border: none; /* 去除按钮边框 */
+            border-radius: 5px; /* 设置按钮圆角 */
+        }
+
+            .custom-button:hover {
+                background-color: #0056b3; /* 设置按钮鼠标悬停时的背景颜色 */
+            }
+    </style>
+    <script>    
+        function btn4_Click(sender) {
+            //從前端開始視窗
+            //sender為註冊是由哪個視窗開啟，作為事後要觸發哪個元件的依據
+            //OpenDialogResult為關閉視後會執行的JS Function
+            //參數使用JSON格式傳遞
+            $uof.dialog.open2("", sender, "", 800, 600, OpenDialogResult, {});
+
+            return false;
+
+        }
+
+        //如果有設定回傳值則執行sender Event
+        function OpenDialogResult(returnValue) {
+            if (typeof (returnValue) == "undefined")
+                return false;
+            else
+                return true;
+        }
+
+
+
+    </script>
+
+    <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+        <ContentTemplate>
+            <telerik:RadTabStrip ID="RadTabStrip1" runat="server"></telerik:RadTabStrip>
+            <telerik:RadTabStrip ID="RadTabStrip2" runat="server" MultiPageID="RadMultiPage" SelectedIndex="0">
+                <Tabs>
+                    <telerik:RadTab Text="明細">
+                    </telerik:RadTab>
+                    <telerik:RadTab Text="資料">
+                    </telerik:RadTab>
+                </Tabs>
+            </telerik:RadTabStrip>
+            <telerik:RadMultiPage ID="RadMultiPage" runat="server" SelectedIndex="0">
+                <telerik:RadPageView ID="RadPageView1" runat="server" Selected="true">
+                    <div id="tabs-1">                        
+                        <table class="PopTable">                            
+                            <tr>
+                                <td class="PopTableLeftTD"></td>
+                                <td>
+                                    <asp:Button ID="Button1" runat="server" Text=" 查詢 " OnClick="Button1_Click"
+                                        meta:resourcekey="btn1Resource1" />
+                                </td>
+
+                            </tr>
+                        </table>                        
+                        <table class="PopTable">
+                            <tr>
+                                <td colspan="2" class="PopTableRightTD">
+                                    <div style="overflow-x: auto; width: 100%">
+                                          <Fast:Grid ID="Grid1" OnRowDataBound="Grid1_RowDataBound" OnRowCommand="Grid1_OnRowCommand" runat="server" OnBeforeExport="OnBeforeExport1" AllowPaging="true" AutoGenerateCheckBoxColumn="False" AllowSorting="True" AutoGenerateColumns="False" CustomDropDownListPage="False" DataKeyOnClientWithCheckBox="False" DefaultSortDirection="Ascending" EmptyDataText="No data found" EnhancePager="True" KeepSelectedRows="False" PageSize="100" SelectedRowColor="" UnSelectedRowColor="" meta:resourcekey="Grid1Resource1" OnPageIndexChanging="grid1_PageIndexChanging">
+                                            <EnhancePagerSettings FirstImageUrl="" FirstAltImageUrl="" PreviousImageUrl="" NextImageUrl="" LastImageUrl="" LastAltImage="" PageNumberCssClass="" PageNumberCurrentCssClass="" PageInfoCssClass="" PageRedirectCssClass="" NextIAltImageUrl="" PreviousAltImageUrl="" ShowHeaderPager="True"></EnhancePagerSettings>
+                                            <ExportExcelSettings AllowExportToExcel="true" ExportType="DataSource"></ExportExcelSettings>
+                                            <Columns>
+                                                <asp:TemplateField HeaderText="表單名稱" ItemStyle-Width="10%" HeaderStyle-HorizontalAlign="Center" ItemStyle-HorizontalAlign="Left">
+                                                    <ItemTemplate>
+                                                        <asp:Label ID="表單名稱" runat="server" Text='<%# Bind("表單名稱") %>' Style="word-break: break-all; white-space: pre-line;"></asp:Label>
+                                                    </ItemTemplate>
+                                                </asp:TemplateField>                                              
+                                            </Columns>
+                                        </Fast:Grid>
+                                    </div>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </telerik:RadPageView>
+                <telerik:RadPageView ID="RadPageView99" runat="server">
+                    <div id="tabs-99">
+                    </div>
+                </telerik:RadPageView>
+            </telerik:RadMultiPage>​
+        </ContentTemplate>
+    </asp:UpdatePanel>
+
+</asp:Content>
+
---
+++ WebPage/COP/SALES_UOF_FORMS.aspx.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Ede.Uof.EIP.SystemInfo;
+using Ede.Uof.Utility.Data;
+using Ede.Uof.Utility.Page.Common;
+using Telerik.Web.UI;
+
+public partial class CDS_WebPage_COP_SALES_UOF_FORMS : Ede.Uof.Utility.Page.BasePage
+{
+    string ACCOUNTS = null;
+    string NAMES = null;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+
+        }
+    }
+
+
+    #region FUNCTION
+    private void BindGrid()
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["connectionstringUOF"].ToString();
+        Ede.Uof.Utility.Data.DatabaseHelper m_db = new Ede.Uof.Utility.Data.DatabaseHelper(connectionString);
+
+        StringBuilder cmdTxt = new StringBuilder();
+        StringBuilder QUERYS1 = new StringBuilder();
+        StringBuilder QUERYS2 = new StringBuilder();
+        StringBuilder QUERYS3 = new StringBuilder();
+        StringBuilder QUERYS4 = new StringBuilder();
+
+        ////
+        //if (!string.IsNullOrEmpty(TextBox1.Text) && !string.IsNullOrEmpty(TextBox1.Text))
+        //{
+        //    QUERYS1.AppendFormat(@" AND [SALESTO] LIKE '%{0}%'", TextBox1.Text.Trim());
+        //}
+        //else
+        //{
+        //    QUERYS1.AppendFormat(@" ");
+        //}
+
+
+        cmdTxt.AppendFormat(@"                            
+                            SELECT 
+                                f.FORM_NAME       AS '表單名稱',
+                                u.NAME            AS '申請者',
+                                CONVERT(nvarchar,t.BEGIN_TIME,111)      AS '申請時間',
+                                t.DOC_NBR         AS '表單編號',   
+                                (
+                                    SELECT STUFF(
+                                        (
+                                            SELECT ',' + u2.NAME
+                                            FROM [UOF].dbo.TB_WKF_TASK_NODE AS TN
+                                            LEFT JOIN [UOF].dbo.TB_EB_USER AS u2
+                                                ON u2.USER_GUID = TN.ORIGINAL_SIGNER
+                                            WHERE TN.SITE_ID = t.CURRENT_SITE_ID
+                                            FOR XML PATH(''), TYPE
+                                        ).value('.', 'nvarchar(max)')
+                                    ,1,1,'')
+                                ) AS '目前簽核者',
+                                CASE 
+                                    WHEN f.FORM_NAME = '1001.文宣樣品申請' 
+                                        THEN t.CURRENT_DOC.value('(/Form/FormFieldValue/FieldItem[@fieldId=""00010""]/@fieldValue)[1]', 'nvarchar(max)')  
+                                    WHEN f.FORM_NAME = '1001.品號申請單' 
+                               THEN STUFF((
+    	                            SELECT N',' + C.value('@fieldValue','nvarchar(200)')
+    	                            FROM CURRENT_DOC.nodes('/Form/FormFieldValue/FieldItem[@fieldId=""RDFrm001IT""]/DataGrid/Row/Cell[@fieldId=""RDFrm001IN""]') AS T(C)
+    	                            FOR XML PATH(''), TYPE
+                                ).value('.', 'nvarchar(max)'), 1, 1, '')           
+                              WHEN f.FORM_NAME = '1001.工作交付單' 
+                               THEN [UOF].dbo.StripHTML(t.CURRENT_DOC.value('(/Form/FormFieldValue/FieldItem[@fieldId=""00010""]/@fieldValue)[1]', 'nvarchar(max)'))
+                                 WHEN f.FORM_NAME = '1002.客訴異常處理單' 
+                                        THEN t.CURRENT_DOC.value('(/Form/FormFieldValue/FieldItem[@fieldId=""QCFrm002PRD""]/@fieldValue)[1]', 'nvarchar(max)') 
+                              WHEN f.FORM_NAME = '1004.無品號試吃製作申請單' 
+                               THEN STUFF((
+                                SELECT N',' + C.value('@fieldValue','nvarchar(200)')
+                                FROM CURRENT_DOC.nodes('/Form/FormFieldValue/FieldItem[@fieldId=""DETAILS""]/DataGrid/Row/Cell[@fieldId=""DVV01""]') AS T(C)
+                                FOR XML PATH(''), TYPE
+                               ).value('.', 'nvarchar(max)'), 1, 1, '')
+                                    WHEN f.FORM_NAME = '1004.品保業務工作申請單' 
+                                        THEN [UOF].dbo.StripHTML(t.CURRENT_DOC.value('(/Form/FormFieldValue/FieldItem[@fieldId=""QCFrm004Process""]/@fieldValue)[1]', 'nvarchar(max)'))
+                              WHEN f.FORM_NAME = '1005 研發業務工作申請單' 
+                               THEN  [UOF].dbo.StripHTML(t.CURRENT_DOC.value('(/Form/FormFieldValue/FieldItem[@fieldId=""RDFrm1003RS""]/@fieldValue)[1]', 'nvarchar(max)'))
+                              WHEN f.FORM_NAME = '1005.舊品變更申請單' 
+                                        THEN t.CURRENT_DOC.value('(/Form/FormFieldValue/FieldItem[@fieldId=""RDFrm1002PD""]/@fieldValue)[1]', 'nvarchar(max)')  
+                              WHEN f.FORM_NAME = '1006.委外送驗申請單' 
+                                        THEN t.CURRENT_DOC.value('(/Form/FormFieldValue/FieldItem[@fieldId=""QC6007""]/DataGrid/Row[@order=""0""]/Cell[@fieldId=""QC60071""]/@fieldValue)[1]', 'nvarchar(200)')
+                              WHEN f.FORM_NAME = '1008.無品號-烘培試吃製作申請單' 
+                               THEN  [UOF].dbo.StripHTML(t.CURRENT_DOC.value('(/Form/FormFieldValue/FieldItem[@fieldId=""DV09""]/@fieldValue)[1]', 'nvarchar(max)'))
+                              WHEN f.FORM_NAME = '2001.產品開發+包裝設計申請單' 
+                                        THEN t.CURRENT_DOC.value('(/Form/FormFieldValue/FieldItem[@fieldId=""FIELD3""]/@fieldValue)[1]', 'nvarchar(max)')  
+                                    ELSE NULL
+                                END AS '表單標題',
+                                t.CURRENT_SITE_ID,
+                                t.CURRENT_DOC
+                            FROM [UOF].dbo.TB_WKF_TASK AS t WITH (NOLOCK)
+                            LEFT JOIN [UOF].dbo.TB_EB_USER AS u
+                                ON u.USER_GUID = t.USER_GUID
+                            LEFT JOIN [UOF].dbo.TB_EB_EMPL_DEP AS ed
+                                ON ed.USER_GUID = u.USER_GUID AND ed.ORDERS = '0'
+                            JOIN [UOF].dbo.TB_WKF_FORM_VERSION AS fv
+                                ON t.FORM_VERSION_ID = fv.FORM_VERSION_ID
+                            JOIN [UOF].dbo.TB_WKF_FORM AS f
+                                ON f.FORM_ID = fv.FORM_ID
+                            WHERE 1=1
+                              AND ISNULL(t.TASK_STATUS,'') NOT IN ('2','3')
+                              AND t.BEGIN_TIME >= '2025-01-01'
+                              AND f.FORM_NAME COLLATE Chinese_Taiwan_Stroke_BIN IN (
+                                    SELECT [FORM_NAME]
+                                    FROM [192.168.1.105].[TKMQ].[dbo].[TB_UOF_SALES_FORM_NAME]
+                              )
+                              AND u.ACCOUNT COLLATE Chinese_Taiwan_Stroke_BIN IN (
+                                    SELECT [ID]
+                                    FROM [192.168.1.105].[TKMQ].[dbo].[TB_UOF_SALES_NAMES]
+                              )
+                            ORDER BY f.FORM_NAME, u.NAME,t.DOC_NBR;
+
+                        ", QUERYS1.ToString(), QUERYS2.ToString(), QUERYS3.ToString(), QUERYS4.ToString());
+
+
+        //m_db.AddParameter("@SDATE", SDATE);
+        //m_db.AddParameter("@EDATE", EDATE);
+
+        DataTable dt = new DataTable();
+
+        dt.Load(m_db.ExecuteReader(cmdTxt.ToString()));
+
+        Grid1.DataSource = dt;
+        Grid1.DataBind();
+
+        MsgBox("完成 \r\n", this.Page, this);
+    }
+
+    protected void grid1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        //Grid1.PageIndex = e.NewPageIndex;
+        //BindGrid();
+    }
+    protected void Grid1_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+
+    }
+
+    protected void Grid1_OnRowCommand(object sender, GridViewCommandEventArgs e)
+    {
+        int rowIndex = -1;
+    }
+
+    public void OnBeforeExport1(object sender, Ede.Uof.Utility.Component.BeforeExportEventArgs e)
+    {
+
+    }
+    public void MsgBox(string ex, Page pg, object obj)
+    {
+        string script = "alert('" + ex.Replace("\r\n", "\\n").Replace("'", "") + "');";
+        ScriptManager.RegisterStartupScript(pg, obj.GetType(), "AlertScript", script, true);
+    }
+
+    #endregion
+
+    #region BUTTON
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        BindGrid();
+    }
+    #endregion
+}
