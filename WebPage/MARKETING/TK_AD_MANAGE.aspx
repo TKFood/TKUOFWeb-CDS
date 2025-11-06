0dab5a3eecb46c9620e391de2efe2fded81aea9f
TKFood <tk290@tkfood.com.tw>
2025-11-06 11:16:04 +0800

    ADD

---
+++ WebPage/MARKETING/TK_AD_MANAGE.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master/DefaultMasterPage.master" AutoEventWireup="true" CodeFile="TK_AD_MANAGE.aspx.cs" Inherits="CDS_WebPage_MARKETING_TK_AD_MANAGE" %>
+
+<%@ Register Assembly="Ede.Uof.Utility.Component.Grid" Namespace="Ede.Uof.Utility.Component" TagPrefix="Fast" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+    <script>    
+
+
+</script>
+
+    <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+        <ContentTemplate>
+            <table class="PopTable">
+
+            </table>
+        </ContentTemplate>
+    </asp:UpdatePanel>
+</asp:Content>
+
---
+++ WebPage/MARKETING/TK_AD_MANAGE.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.Linq;
+using Ede.Uof.EIP.SystemInfo;
+using Ede.Uof.Utility.Data;
+using Ede.Uof.Utility.Log;
+using Ede.Uof.Utility.Page.Common;
+using OfficeOpenXml;
+using OfficeOpenXml.Drawing;
+using OfficeOpenXml.Style;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+public partial class CDS_WebPage_MARKETING_TK_AD_MANAGE : Ede.Uof.Utility.Page.BasePage
+{
+    string ACCOUNT = null;
+    string NAME = null;
+    String ROLES = null;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        ACCOUNT = Current.Account;
+        NAME = Current.User.Name;
+    }
+
+    #region FUNCTION
+
+    #endregion
+
+    #region BUTTON
+
+    #endregion
+}
