82c3fbd7c80108c3cdea0f8ce2a31a826a34d512
TKFood <tk290@tkfood.com.tw>
2026-05-05 09:55:22 +0800

    new2

--- WebPage/COP/TB_SALES_ASSINGED_CN.aspx.cs
+++ WebPage/COP/TB_SALES_ASSINGED_CN.aspx.cs
@@ -32,11 +32,280 @@ public partial class CDS_WebPage_COP_TB_SALES_ASSINGED_CN : Ede.Uof.Utility.Page
 
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+        {
+            txtDate1.Text = DateTime.Now.ToString("yyyy/MM/dd");
+            BindDropDownList1();
+            BindDropDownList2();
+            BindDropDownList3();
+            BindDropDownList4();
 
+            BindDropDownListISCLOSE();
+            BindDropDownListISCLOSE2();
+            BindDropDownListISCLOSE3();
+
+            //BindGrid();
+            //BindGrid2();
+            //BindGrid3();
+
+        }
     }
 
     #region FUNCTION
+    private void BindDropDownList1()
+    {
+        DataTable dt = new DataTable();
+        dt.Columns.Add("ID", typeof(String));
+        dt.Columns.Add("KINDS", typeof(String));
 
+        string connectionString = ConfigurationManager.ConnectionStrings["ERPconnectionstring"].ToString();
+        Ede.Uof.Utility.Data.DatabaseHelper m_db = new Ede.Uof.Utility.Data.DatabaseHelper(connectionString);
+
+        string cmdTxt = @" 
+                        SELECT 
+                        [ID]
+                        ,[NAME]
+                        ,[LEADER]
+                        FROM [TKBUSINESS].[dbo].[TB_SALESNAME_CN]
+                        WHERE [NAME] NOT IN ('全部')
+                        ";
+
+        dt.Load(m_db.ExecuteReader(cmdTxt));
+
+        if (dt.Rows.Count > 0)
+        {
+            DropDownList1.DataSource = dt;
+            DropDownList1.DataTextField = "NAME";
+            DropDownList1.DataValueField = "NAME";
+            DropDownList1.DataBind();
+
+        }
+        else
+        {
+
+        }
+    }
+    private void BindDropDownList2()
+    {
+        DataTable dt = new DataTable();
+        dt.Columns.Add("ID", typeof(String));
+        dt.Columns.Add("KINDS", typeof(String));
+
+        string connectionString = ConfigurationManager.ConnectionStrings["ERPconnectionstring"].ToString();
+        Ede.Uof.Utility.Data.DatabaseHelper m_db = new Ede.Uof.Utility.Data.DatabaseHelper(connectionString);
+
+        string cmdTxt = @" 
+                        SELECT 
+                        [ID]
+                        ,[KINDS]
+                        ,[NAMES]
+                        ,[VALUE]
+                        FROM [TKBUSINESS].[dbo].[TBPARA]
+                        WHERE [KINDS]='是否結案'
+                        ORDER BY [ID]
+                        ";
+
+        dt.Load(m_db.ExecuteReader(cmdTxt));
+
+        if (dt.Rows.Count > 0)
+        {
+            DropDownList2.DataSource = dt;
+            DropDownList2.DataTextField = "NAMES";
+            DropDownList2.DataValueField = "NAMES";
+            DropDownList2.DataBind();
+
+        }
+        else
+        {
+
+        }
+    }
+    private void BindDropDownList3()
+    {
+        DataTable dt = new DataTable();
+        dt.Columns.Add("ID", typeof(String));
+        dt.Columns.Add("KINDS", typeof(String));
+
+        string connectionString = ConfigurationManager.ConnectionStrings["ERPconnectionstring"].ToString();
+        Ede.Uof.Utility.Data.DatabaseHelper m_db = new Ede.Uof.Utility.Data.DatabaseHelper(connectionString);
+
+        string cmdTxt = @" 
+                        SELECT  
+                        [ID]
+                        ,[KINDS]
+                        ,[NAMES]
+                        ,[VALUE]
+                        FROM [TKBUSINESS].[dbo].[TBPARA]
+                        WHERE [KINDS]='TB_SALES_ASSINGED'
+                        ORDER BY [ID]
+                        ";
+
+        dt.Load(m_db.ExecuteReader(cmdTxt));
+
+        if (dt.Rows.Count > 0)
+        {
+            DropDownList3.DataSource = dt;
+            DropDownList3.DataTextField = "NAMES";
+            DropDownList3.DataValueField = "NAMES";
+            DropDownList3.DataBind();
+
+        }
+        else
+        {
+
+        }
+    }
+
+    private void BindDropDownList4()
+    {
+        DataTable dt = new DataTable();
+        dt.Columns.Add("ID", typeof(String));
+        dt.Columns.Add("KINDS", typeof(String));
+
+        string connectionString = ConfigurationManager.ConnectionStrings["ERPconnectionstring"].ToString();
+        Ede.Uof.Utility.Data.DatabaseHelper m_db = new Ede.Uof.Utility.Data.DatabaseHelper(connectionString);
+
+        string cmdTxt = @" 
+                        SELECT *
+                        FROM 
+                        (
+                        SELECT 
+                        [ID]
+                        ,[NAME]
+                        ,[LEADER]
+                        FROM [TKBUSINESS].[dbo].[TB_SALESNAME_CN]
+                        WHERE [NAME] NOT IN ('全部')
+                        UNION ALL
+                        SELECT 
+                        '' [ID]
+                        ,'' [NAME]
+                        ,'' [LEADER]
+                        ) AS TMEP
+                        ORDER BY [ID]
+
+                        ";
+
+        dt.Load(m_db.ExecuteReader(cmdTxt));
+
+        if (dt.Rows.Count > 0)
+        {
+            DropDownList4.DataSource = dt;
+            DropDownList4.DataTextField = "NAME";
+            DropDownList4.DataValueField = "NAME";
+            DropDownList4.DataBind();
+
+        }
+        else
+        {
+
+        }
+    }
+    private void BindDropDownListISCLOSE()
+    {
+        DataTable dt = new DataTable();
+        dt.Columns.Add("ID", typeof(String));
+        dt.Columns.Add("KINDS", typeof(String));
+
+        string connectionString = ConfigurationManager.ConnectionStrings["ERPconnectionstring"].ToString();
+        Ede.Uof.Utility.Data.DatabaseHelper m_db = new Ede.Uof.Utility.Data.DatabaseHelper(connectionString);
+
+        string cmdTxt = @" 
+                        SELECT 
+                        [ID]
+                        ,[KINDS]
+                        ,[NAMES]
+                        ,[VALUE]
+                        FROM [TKBUSINESS].[dbo].[TBPARA]
+                        WHERE [KINDS]='是否結案'
+                        ORDER BY [ID]
+                        ";
+
+        dt.Load(m_db.ExecuteReader(cmdTxt));
+
+        if (dt.Rows.Count > 0)
+        {
+            DropDownListISCLOSE.DataSource = dt;
+            DropDownListISCLOSE.DataTextField = "NAMES";
+            DropDownListISCLOSE.DataValueField = "NAMES";
+            DropDownListISCLOSE.DataBind();
+
+        }
+        else
+        {
+
+        }
+    }
+
+    private void BindDropDownListISCLOSE2()
+    {
+        DataTable dt = new DataTable();
+        dt.Columns.Add("ID", typeof(String));
+        dt.Columns.Add("KINDS", typeof(String));
+
+        string connectionString = ConfigurationManager.ConnectionStrings["ERPconnectionstring"].ToString();
+        Ede.Uof.Utility.Data.DatabaseHelper m_db = new Ede.Uof.Utility.Data.DatabaseHelper(connectionString);
+
+        string cmdTxt = @" 
+                        SELECT 
+                        [ID]
+                        ,[KINDS]
+                        ,[NAMES]
+                        ,[VALUE]
+                        FROM [TKBUSINESS].[dbo].[TBPARA]
+                        WHERE [KINDS]='是否結案'
+                        ORDER BY [ID]
+                        ";
+
+        dt.Load(m_db.ExecuteReader(cmdTxt));
+
+        if (dt.Rows.Count > 0)
+        {
+            DropDownListISCLOSE2.DataSource = dt;
+            DropDownListISCLOSE2.DataTextField = "NAMES";
+            DropDownListISCLOSE2.DataValueField = "NAMES";
+            DropDownListISCLOSE2.DataBind();
+
+        }
+        else
+        {
+
+        }
+    }
+    private void BindDropDownListISCLOSE3()
+    {
+        DataTable dt = new DataTable();
+        dt.Columns.Add("ID", typeof(String));
+        dt.Columns.Add("KINDS", typeof(String));
+
+        string connectionString = ConfigurationManager.ConnectionStrings["ERPconnectionstring"].ToString();
+        Ede.Uof.Utility.Data.DatabaseHelper m_db = new Ede.Uof.Utility.Data.DatabaseHelper(connectionString);
+
+        string cmdTxt = @" 
+                        SELECT 
+                        [ID]
+                        ,[KINDS]
+                        ,[NAMES]
+                        ,[VALUE]
+                        FROM [TKBUSINESS].[dbo].[TBPARA]
+                        WHERE [KINDS]='是否結案'
+                        ORDER BY [ID]
+                        ";
+
+        dt.Load(m_db.ExecuteReader(cmdTxt));
+
+        if (dt.Rows.Count > 0)
+        {
+            DropDownListISCLOSE3.DataSource = dt;
+            DropDownListISCLOSE3.DataTextField = "NAMES";
+            DropDownListISCLOSE3.DataValueField = "NAMES";
+            DropDownListISCLOSE3.DataBind();
+
+        }
+        else
+        {
+
+        }
+    }
 
     public void MsgBox(String ex, Page pg, Object obj)
     {
